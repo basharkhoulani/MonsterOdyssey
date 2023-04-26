726b1b6c620df239fa1c169d25a7261bf1fbc972
olottko <olottko@gmail.com>
2023-04-26 23:11:24 +0200

    refactor: App

--- src/main/java/de/uniks/stpmon/team_m/App.java
+++ src/main/java/de/uniks/stpmon/team_m/App.java
@@ -48,7 +48,7 @@ public class App extends Application {
             return;
         }
         final MainComponent component = DaggerMainComponent.builder().mainApp(this).build();
-        // controller = component.loginController();
+        // controller = component.login....
         // initAndRender(controller);
     }
 
