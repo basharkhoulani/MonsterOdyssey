012b068c449000c774613a79a2aa6a79af113091
olottko <olottko@gmail.com>
2023-04-26 20:36:32 +0200

    feat: Add Main and App

---
+++ src/main/java/de/uniks/stpmon/team_m/App.java
@@ -0,0 +1,11 @@
+package de.uniks.stpmon.team_m;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
