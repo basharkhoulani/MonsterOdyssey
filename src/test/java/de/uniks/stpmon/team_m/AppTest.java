ba421fbf9479e248eaff4b9d097f880fbda17ee4
olottko <olottko@gmail.com>
2023-04-26 21:26:50 +0200

    test: Add AppTest
    WIP: currently useless

---
+++ src/test/java/de/uniks/stpmon/team_m/AppTest.java
@@ -0,0 +1,26 @@
+package de.uniks.stpmon.team_m;
+
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest extends ApplicationTest {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        new App().start(stage);
+    }
+
+     @Test
+    void testLoading(){
+        final Label loading = lookup("Loading...").query();
+        assertNotNull(loading);
+     }
+
+}
