1d1e9cba96e4a2cf25fc5612d9dddbfb49fad8e0
olottko <olottko@gmail.com>
2023-04-26 20:51:02 +0200

    feat: Stage init

--- src/main/java/de/uniks/stpmon/team_m/App.java
+++ src/main/java/de/uniks/stpmon/team_m/App.java
@@ -1,11 +1,37 @@
 package de.uniks.stpmon.team_m;
 
+import de.uniks.stpmon.team_m.controller.Controller;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    private Stage stage;
+    private Controller controller;
+
+    public App(){
+
+    }
+
+    public App(Controller controller) {
+        this.controller = controller;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        stage.setWidth(640);
+        stage.setHeight(480);
+        stage.setTitle("Monster Odyssey");
 
+        final Scene scene = new Scene(new Label("Loading..."));
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
