39404e2f913cf3b59cc0365d1d0cecaa35137baf
olottko <olottko@gmail.com>
2023-04-27 20:11:36 +0200

    feat: Add UpdateUserDto, update and delete

--- src/main/java/de/uniks/stpmon/team_m/rest/UserApiService.java
+++ src/main/java/de/uniks/stpmon/team_m/rest/UserApiService.java
@@ -1,6 +1,7 @@
 package de.uniks.stpmon.team_m.rest;
 
 import de.uniks.stpmon.team_m.dto.CreateUserDto;
+import de.uniks.stpmon.team_m.dto.UpdateUserDto;
 import de.uniks.stpmon.team_m.dto.User;
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.*;
@@ -14,7 +15,8 @@ public interface UserApiService {
     Observable<User[]> getUsers(@Query("ids") String[] ids, @Query("status") String status);
     @GET("users/{id}")
     Observable<User> getUser(@Path("id") String id);
-
-
-
+    @PATCH("users/{id")
+    Observable<User> update(@Body UpdateUserDto dto);
+    @DELETE("users/{id}")
+    Observable<User> delete(@Path("id") String id);
 }
