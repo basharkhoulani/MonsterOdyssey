3955230ea2735c8ad8ba18f58fd6bf1d69d0e97f
Cheng-JY <chengjiaying.prc@gmail.com>
2023-04-27 20:38:51 +0200

    Add the Eye Icon for showing password

--- src/main/java/de/uniks/stpmon/team_m/controller/LoginController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/LoginController.java
@@ -1,10 +1,16 @@
 package de.uniks.stpmon.team_m.controller;
 
 
+import de.uniks.stpmon.team_m.App;
+import de.uniks.stpmon.team_m.Main;
 import javafx.beans.binding.BooleanBinding;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import javax.inject.Inject;
@@ -29,8 +35,14 @@ public class LoginController extends Controller{
     public Parent render() {
         final Parent parent = super.render();
 
+        Image hideEye = new Image(getClass().getResource("../views/hideEye.png").toString());
+        hideImage.setImage(hideEye);
+
+
         return parent;
     }
+
+
 
     public void signIn(){
 
