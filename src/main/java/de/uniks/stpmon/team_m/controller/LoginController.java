3187633250f206ec08a403eeb166da32b070f04b
Cheng-JY <chengjiaying.prc@gmail.com>
2023-04-27 15:42:55 +0200

    feat Add empty LoginController Add Login.fxml

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/LoginController.java
@@ -0,0 +1,11 @@
+package de.uniks.stpmon.team_m.controller;
+
+
+import javax.inject.Inject;
+
+public class LoginController extends Controller{
+    @Inject
+    public LoginController(){
+
+    }
+}
