10c0890427c5a51ef0cfd670cea729c0f7b98500
olottko <olottko@gmail.com>
2023-04-27 20:05:17 +0200

    feat: Add getUsers and getUser

--- src/main/java/de/uniks/stpmon/team_m/rest/UserApiService.java
+++ src/main/java/de/uniks/stpmon/team_m/rest/UserApiService.java
@@ -3,10 +3,18 @@ package de.uniks.stpmon.team_m.rest;
 import de.uniks.stpmon.team_m.dto.CreateUserDto;
 import de.uniks.stpmon.team_m.dto.User;
 import io.reactivex.rxjava3.core.Observable;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
+import retrofit2.http.*;
 
 public interface UserApiService {
     @POST("users")
     Observable<User> create(@Body CreateUserDto dto);
+
+    // For all users pass null for both parameters. Also only one parameter can be null.
+    @GET("users")
+    Observable<User[]> getUsers(@Query("ids") String[] ids, @Query("status") String status);
+    @GET("users/{id}")
+    Observable<User> getUser(@Path("id") String id);
+
+
+
 }
