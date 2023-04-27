aeaa3e1aaad7eaf9660e9980c5e05b23515d2466
Bashar Khoulani <uk083781@student.uni-kassel.de>
2023-04-27 11:19:33 +0200

    feat: Add basic structure for MainMenuController

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/MainMenuController.java
@@ -0,0 +1,48 @@
+package de.uniks.stpmon.team_m.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class MainMenuController extends Controller {
+
+
+    @FXML
+    public AnchorPane friendsListScrollPane;
+    @FXML
+    public Button findNewFriendsButton;
+    @FXML
+    public Button messagesButton;
+    @FXML
+    public Button logoutButton;
+    @FXML
+    public Button settingsButton;
+    @FXML
+    public Button startGameButton;
+    @FXML
+    public VBox regionRadioButtonList;
+
+    @Override
+    public Parent render() {
+        final Parent parent = super.render();
+        return parent;
+    }
+
+
+    public void changeToFindNewFriends() {
+    }
+
+    public void changeToMessages() {
+    }
+
+    public void changeToLogin() {
+    }
+
+    public void changeToSettings() {
+    }
+
+    public void changeToIngame() {
+    }
+}
