1d75d8066ca5b1760e39f419e077047d279db108
olottko <olottko@gmail.com>
2023-04-28 18:54:48 +0200

    fix: Change Array to List

--- src/main/java/de/uniks/stpmon/team_m/rest/GroupsApiService.java
+++ src/main/java/de/uniks/stpmon/team_m/rest/GroupsApiService.java
@@ -4,18 +4,20 @@ import de.uniks.stpmon.team_m.dto.*;
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.*;
 
+import java.util.List;
+
 public interface GroupsApiService {
     @POST("groups")
     Observable<Group> create(@Body CreateGroupDto dto);
 
     // For all groups the current user is member of, pass null as the parameter.
     @GET("groups")
-    Observable<Group[]> getGroups(@Query("ids") String[] ids);
+    Observable<Group[]> getGroups(@Query("ids") List<String> ids);
 
     @GET("groups/{id}")
     Observable<Group> getUser(@Path("id") String id);
 
-    @PATCH("groups/{id")
+    @PATCH("groups/{id}")
     Observable<Group> update(@Body UpdateGroupDto dto);
 
     @DELETE("groups/{id}")
