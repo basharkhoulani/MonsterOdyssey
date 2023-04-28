52bf36834edde7bb3916fa498b0030963dd59d0c
Cheng-JY <chengjiaying.prc@gmail.com>
2023-04-28 19:35:24 +0200

    feat: Add the show password Button

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/PasswordFieldSkin.java
@@ -0,0 +1,34 @@
+package de.uniks.stpmon.team_m.controller.subController;
+
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.skin.TextFieldSkin;
+
+
+public class PasswordFieldSkin extends TextFieldSkin {
+    private boolean mask = true;
+    private static final char BULLET = '\u25cf';
+
+    public PasswordFieldSkin(TextField control) {
+        super(control);
+    }
+
+    @Override
+    protected String maskText(String txt) {
+        if (getSkinnable() instanceof PasswordField && mask) {
+            int n = txt.length();
+            StringBuilder passwordBuilder = new StringBuilder(n);
+            for (int i = 0; i < n; i++) {
+                passwordBuilder.append(BULLET);
+            }
+            return passwordBuilder.toString();
+        } else {
+            return txt;
+        }
+    }
+
+    public boolean getMask(){return this.mask;}
+    public void setMask(boolean mask){
+        this.mask = mask;
+    }
+}
