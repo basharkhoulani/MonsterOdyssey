6340044b14850455e3519645f156516baf06ebf9
olottko <olottko@gmail.com>
2023-04-28 17:57:07 +0200

    feat: Add GroupsApiService + Dtos + Refactor

---
+++ src/main/java/de/uniks/stpmon/team_m/dto/UpdateGroupDto.java
@@ -0,0 +1,7 @@
+package de.uniks.stpmon.team_m.dto;
+
+public record UpdateGroupDto(
+        String name,
+        String[] members
+) {
+}
---
+++ src/main/java/de/uniks/stpmon/team_m/rest/GroupsApiService.java
@@ -0,0 +1,23 @@
+package de.uniks.stpmon.team_m.rest;
+
+import de.uniks.stpmon.team_m.dto.*;
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.*;
+
+public interface GroupsApiService {
+    @POST("groups")
+    Observable<Group> create(@Body CreateGroupDto dto);
+
+    // For all groups the current user is member of, pass null as the parameter.
+    @GET("groups")
+    Observable<Group[]> getGroups(@Query("ids") String[] ids);
+
+    @GET("groups/{id}")
+    Observable<Group> getUser(@Path("id") String id);
+
+    @PATCH("groups/{id")
+    Observable<Group> update(@Body UpdateGroupDto dto);
+
+    @DELETE("groups/{id}")
+    Observable<Group> delete(@Path("id") String id);
+}
