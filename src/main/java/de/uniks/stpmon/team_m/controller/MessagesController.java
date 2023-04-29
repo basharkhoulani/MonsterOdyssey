5882f30fc23bb3dde23852aa15f2a7a849ca738c
sacharzki <leon.sacharzki@octavia.de>
2023-04-29 16:36:49 +0200

    feat(messagesController.java): add fxml imports

--- src/main/java/de/uniks/stpmon/team_m/controller/MessagesController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/MessagesController.java
@@ -1,11 +1,56 @@
 package de.uniks.stpmon.team_m.controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 
 public class MessagesController extends Controller{
+
+    @FXML
+    public VBox leftSideVBox;
+
+    @FXML
+    public VBox rightSideVBox;
+
+    @FXML
+    public Text friendsAndGroupText;
+
+    @FXML
+    public ScrollPane friendsAndGroupsScrollPane;
+
+    @FXML
+    public VBox friendsAndGroupsVBox;
+
+    @FXML
+    public Button findNewFriendsButton;
+
+    @FXML
+    public Button newGroupButton;
+
+    @FXML
+    public Button mainMenuButton;
+
+    @FXML
+    public Text currentFriendOrGroupText; //needs to be set each time a different chat is selected
+
+    @FXML
+    public ScrollPane chatScrollPane;
+
+    @FXML
+    public VBox chatVBox;
+
+    @FXML
+    public TextArea messageTextArea;
+
+    @FXML
+    public Button sendButton;
 
     @Inject
     Provider<MainMenuController> mainMenuControllerProvider;
