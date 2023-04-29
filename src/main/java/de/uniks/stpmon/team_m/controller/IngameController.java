eba4e1ccb2510f5f74c41f6f472b07f4fae7b0b5
Bashar Khoulani <uk083781@student.uni-kassel.de>
2023-04-29 14:45:59 +0200

    feat: Add help button functionality

--- src/main/java/de/uniks/stpmon/team_m/controller/IngameController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/IngameController.java
@@ -1,16 +1,27 @@
 package de.uniks.stpmon.team_m.controller;
 
 
+import de.uniks.stpmon.team_m.controller.subController.IngameHelpController;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 
 public class IngameController extends Controller {
 
     @FXML
     public Button helpSymbol;
+
+    private boolean helpShown = false;
+
+    @Inject
+    Provider<IngameHelpController> ingameHelpControllerProvider;
 
     @Inject
     public IngameController() {
@@ -28,5 +39,15 @@ public class IngameController extends Controller {
     }
 
     public void showHelp() {
+        if (!helpShown) {
+            helpShown = true;
+            final Stage dialog = new Stage();
+            dialog.initModality(Modality.NONE);
+            dialog.initStyle(StageStyle.UNDECORATED);
+            dialog.initOwner(app.getStage());
+            Scene scene = new Scene(ingameHelpControllerProvider.get().render());
+            dialog.setScene(scene);
+            dialog.show();
+        }
     }
 }
--- src/main/java/de/uniks/stpmon/team_m/controller/subController/IngameHelpController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/IngameHelpController.java
@@ -2,13 +2,26 @@ package de.uniks.stpmon.team_m.controller.subController;
 
 import de.uniks.stpmon.team_m.controller.Controller;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+
+import javax.inject.Inject;
 
 public class IngameHelpController extends Controller {
 
 
     @FXML
     public Button backToGameButton;
+
+    @Inject
+    public IngameHelpController() {
+    }
+
+    @Override
+    public Parent render() {
+        final Parent parent = super.render();
+        return parent;
+    }
 
     public void resumeGame() {
 
