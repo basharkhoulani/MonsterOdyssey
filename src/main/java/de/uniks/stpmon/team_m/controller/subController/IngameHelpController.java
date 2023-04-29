00016809b2463796107be2d1416a9d379d3e6713
Bashar Khoulani <uk083781@student.uni-kassel.de>
2023-04-29 14:34:32 +0200

    feat: Add Controller

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/IngameHelpController.java
@@ -0,0 +1,17 @@
+package de.uniks.stpmon.team_m.controller.subController;
+
+import de.uniks.stpmon.team_m.controller.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class IngameHelpController extends Controller {
+
+
+    @FXML
+    public Button backToGameButton;
+
+    public void resumeGame() {
+
+    }
+
+}
