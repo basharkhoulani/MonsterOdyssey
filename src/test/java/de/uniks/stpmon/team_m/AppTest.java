59d9d94bef418c9762672aacc14cb2f824ab5eca
sacharzki <leon.sacharzki@octavia.de>
2023-04-30 16:49:46 +0200

    test: add messages -> newFriends test

--- src/test/java/de/uniks/stpmon/team_m/AppTest.java
+++ src/test/java/de/uniks/stpmon/team_m/AppTest.java
@@ -181,4 +181,25 @@ class AppTest extends ApplicationTest {
         assertNotNull(logoutButton);
     }
 
+    @Test
+    void testMessagesToNewFriends() {
+        // login -> main menu
+        final Button signInButton = lookup("Sign In").query();
+        assertNotNull(signInButton);
+        clickOn(signInButton);
+
+        // main menu -> messages
+        final Button messagesButton = lookup("Messages").query();
+        assertNotNull(messagesButton);
+        clickOn(messagesButton);
+        assertEquals("Monster Odyssey - Messages", stage.getTitle());
+
+        // messages -> new friends
+        final Button findNewFriendsButton = lookup("#findNewFriendsButton").query();
+        assertNotNull(findNewFriendsButton);
+        clickOn(findNewFriendsButton);
+
+        assertEquals("Monster Odyssey - Add a new friend", stage.getTitle());
+    }
+
 }
