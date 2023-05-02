8a5b50c1a427dcbd7dd329cb03050a72e2c4f50c
sacharzki <leon.sacharzki@octavia.de>
2023-05-02 09:26:11 +0200

    test: add check if nodes are displayed correctly

--- src/test/java/de/uniks/stpmon/team_m/AppTest.java
+++ src/test/java/de/uniks/stpmon/team_m/AppTest.java
@@ -3,6 +3,7 @@ package de.uniks.stpmon.team_m;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
@@ -277,6 +278,10 @@ class AppTest extends ApplicationTest {
         clickOn(findNewFriendsButton);
 
         assertEquals("Monster Odyssey - New Group", stage.getTitle());
+        final Button createGroupButton = lookup("Create Group").query();
+        assertNotNull(createGroupButton);
+        final Text selectGroupMembersText = lookup("Select Groupmembers").queryText();
+        assertNotNull(selectGroupMembersText);
     }
 
 }
