dda29ad6690e8e0403c10a3db58a5b71a4ad7967
sacharzki <leon.sacharzki@octavia.de>
2023-05-03 16:23:22 +0200

    feat(messageService): add newMessage method

--- src/main/java/de/uniks/stpmon/team_m/service/MessageService.java
+++ src/main/java/de/uniks/stpmon/team_m/service/MessageService.java
@@ -1,6 +1,7 @@
 package de.uniks.stpmon.team_m.service;
 
 import de.uniks.stpmon.team_m.Constants;
+import de.uniks.stpmon.team_m.dto.CreateMessageDto;
 import de.uniks.stpmon.team_m.dto.Message;
 import de.uniks.stpmon.team_m.rest.MessagesApiService;
 import io.reactivex.rxjava3.core.Observable;
@@ -20,5 +21,10 @@ public class MessageService {
 
     public Observable<List<Message>> getMessagesOfUser(String userID) {
         return messagesApiService.getMessages(Constants.API_URL, userID);
+    }
+
+    public Observable<Message> newMessage(String userID, String message) {
+        CreateMessageDto createMessageDto = new CreateMessageDto(message);
+        return messagesApiService.create(Constants.API_URL, userID, createMessageDto);
     }
 }
