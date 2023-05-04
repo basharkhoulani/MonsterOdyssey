092a093a2fda80712523f70a8a9b133399face2e
Bashar Khoulani <uk083781@student.uni-kassel.de>
2023-05-04 15:15:23 +0200

    feat: Create ground structure UsersService.java

---
+++ src/main/java/de/uniks/stpmon/team_m/service/UsersService.java
@@ -0,0 +1,41 @@
+package de.uniks.stpmon.team_m.service;
+
+import de.uniks.stpmon.team_m.dto.CreateUserDto;
+import de.uniks.stpmon.team_m.dto.UpdateUserDto;
+import de.uniks.stpmon.team_m.dto.User;
+import de.uniks.stpmon.team_m.rest.UsersApiService;
+import io.reactivex.rxjava3.core.Observable;
+
+import javax.inject.Inject;
+import java.util.List;
+
+public class UsersService {
+
+    private final UsersApiService usersApiService;
+
+    @Inject
+    public UsersService(UsersApiService usersApiService) {
+        this.usersApiService = usersApiService;
+    }
+
+    public Observable<User> createUser(String username, String password) {
+        return usersApiService.create(new CreateUserDto(username, password));
+    }
+
+    public Observable<User> getUser(String id) {
+        return usersApiService.getUser(id);
+    }
+
+    // For all users pass null for both parameters. Also only one parameter can be null.
+    public Observable<List<User>> getUsers(List<String> ids, String status) {
+        return usersApiService.getUsers(ids, status);
+    }
+
+    public Observable<User> updateUser(String name, String status, List<String> friends, String password) {
+        return usersApiService.update(new UpdateUserDto(name, status, friends, password));
+    }
+
+    public Observable<User> deleteUSer(User user) {
+        return usersApiService.delete(user._id());
+    }
+}
