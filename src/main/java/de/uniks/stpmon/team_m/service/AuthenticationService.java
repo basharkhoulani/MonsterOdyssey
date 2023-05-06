cbe0fb4f51e5270925379ea8e68357c8c575a3b5
sacharzki <leon.sacharzki@octavia.de>
2023-05-06 19:12:11 +0200

    feat(AuthService): add methods for rememberMe functionality

--- src/main/java/de/uniks/stpmon/team_m/service/AuthenticationService.java
+++ src/main/java/de/uniks/stpmon/team_m/service/AuthenticationService.java
@@ -1,28 +1,48 @@
 package de.uniks.stpmon.team_m.service;
 
+import de.uniks.stpmon.team_m.Constants;
 import de.uniks.stpmon.team_m.dto.LoginDto;
 import de.uniks.stpmon.team_m.dto.LoginResult;
 import de.uniks.stpmon.team_m.dto.LogoutResult;
+import de.uniks.stpmon.team_m.dto.RefreshDto;
 import de.uniks.stpmon.team_m.rest.AuthApiService;
 import io.reactivex.rxjava3.core.Observable;
 
 import javax.inject.Inject;
+import java.util.prefs.Preferences;
 
 public class AuthenticationService {
     private final TokenStorage tokenStorage;
     private final AuthApiService authApiService;
+    private final Preferences preferences;
 
     @Inject
     public AuthenticationService(
             TokenStorage tokenStorage,
-            AuthApiService authApiService
+            AuthApiService authApiService,
+            Preferences preferences
     ) {
         this.tokenStorage = tokenStorage;
         this.authApiService = authApiService;
+        this.preferences = preferences;
     }
 
-    public Observable<LoginResult> login(String username, String password) {
+    public Observable<LoginResult> login(String username, String password, boolean rememberMe) {
         return authApiService.login(new LoginDto(username, password)).map(lr -> {
+            tokenStorage.setToken(lr.accessToken());
+            if (rememberMe) {
+                preferences.put(Constants.REFRESH_TOKEN_PREF, lr.refreshToken());
+            }
+            return lr;
+        });
+    }
+
+    public boolean isRememberMe() {
+        return preferences.get(Constants.REFRESH_TOKEN_PREF, null) != null;
+    }
+
+    public Observable<LoginResult> refresh() {
+        return  authApiService.refresh(new RefreshDto(preferences.get(Constants.REFRESH_TOKEN_PREF, null))).map(lr -> {
             tokenStorage.setToken(lr.accessToken());
             return lr;
         });
