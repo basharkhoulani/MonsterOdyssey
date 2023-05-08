ae85e990a2a44220c2f50951872958eafd8f3cf6
sacharzki <leon.sacharzki@octavia.de>
2023-05-08 15:58:46 +0200

    add a bit of testing

--- src/main/java/de/uniks/stpmon/team_m/controller/MessagesController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/MessagesController.java
@@ -1,16 +1,29 @@
 package de.uniks.stpmon.team_m.controller;
 
+import de.uniks.stpmon.team_m.Main;
+import de.uniks.stpmon.team_m.dto.User;
 import de.uniks.stpmon.team_m.service.GroupStorage;
+import io.reactivex.rxjava3.core.Observable;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
+
+import java.io.IOException;
+import java.util.List;
 
 import static de.uniks.stpmon.team_m.Constants.*;
 
@@ -24,8 +37,6 @@ public class MessagesController extends Controller {
     public Text friendsAndGroupText;
     @FXML
     public ScrollPane friendsAndGroupsScrollPane;
-    @FXML
-    public VBox friendsAndGroupsVBox;
     @FXML
     public Button findNewFriendsButton;
     @FXML
@@ -44,6 +55,8 @@ public class MessagesController extends Controller {
     public Button sendButton;
     @FXML
     public Button settingsButton;
+    @FXML
+    public VBox friendsAndGroupsVBox;
 
     @Inject
     Provider<MainMenuController> mainMenuControllerProvider;
@@ -68,7 +81,11 @@ public class MessagesController extends Controller {
 
     @Override
     public Parent render() {
-        return super.render();
+        //final Parent parent = FXMLLoader.load(Main.class.getResource("view/Monster.fxml"))
+        Parent parent = super.render();
+        createFriendNode();
+
+        return parent;
     }
 
     @Override
@@ -101,6 +118,34 @@ public class MessagesController extends Controller {
          * The Pane should be a new fxml fragment that gets loaded into one of
          * the VBoxs' children.
          * */
+        System.out.println(friendsAndGroupsVBox);
+        HBox testHbox = new HBox();
+        testHbox.setStyle("-fx-border-color: black; -fx-border-width: 2px; -fx-border-style: solid none none none;");
+        Text testText = new Text();
+        testText.setText("HALLO");
+        testText.setStyle("-fx-background-color: green");
+        testHbox.getChildren().add(testText);
+
+        for (int i = 0; i < 20; i++) {
+            HBox testi = new HBox();
+            testi.setPadding(new Insets(5));
+            Text wombat = new Text();
+            wombat.setText(Integer.toString(i));
+            testi.setPrefHeight(50);
+            Circle status = new Circle();
+            status.setRadius(20);
+            status.setFill(Color.LIGHTGREEN);
+            status.setStroke(Color.BLACK);
+            status.setStrokeWidth(1);
+            testi.getChildren().add(status);
+            testi.getChildren().add(wombat);
+            testi.setMinHeight(50);
+            testi.setStyle("-fx-border-color: black; -fx-border-width: 2px; -fx-border-style: solid none none none;");
+            friendsAndGroupsVBox.getChildren().add(testi);
+            VBox.setVgrow(friendsAndGroupsScrollPane, Priority.ALWAYS);
+        }
+
+        System.out.println(friendsAndGroupsScrollPane.getContent());
     }
 
     public void creteMessageNode() {
