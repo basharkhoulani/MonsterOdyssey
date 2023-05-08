73ed888a23de8499e1c5a37d93c984507191e31d
olottko <olottko@gmail.com>
2023-05-08 18:13:18 +0200

    feat: Change UpdateUserDto, so fields can be null

--- src/main/java/de/uniks/stpmon/team_m/dto/UpdateUserDto.java
+++ src/main/java/de/uniks/stpmon/team_m/dto/UpdateUserDto.java
@@ -1,7 +1,9 @@
 package de.uniks.stpmon.team_m.dto;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.util.List;
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record UpdateUserDto(
         String name,
         String status,
