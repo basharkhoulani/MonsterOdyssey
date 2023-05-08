e926d1c45892e4765ae430b1a040f0dd942561cc
Cheng-JY <chengjiaying.prc@gmail.com>
2023-05-08 22:01:33 +0200

    Add TestComponent and Test Module

---
+++ src/test/java/de/uniks/stpmon/team_m/TestComponent.java
@@ -0,0 +1,15 @@
+package de.uniks.stpmon.team_m;
+
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+@Component(modules = {TestModule.class})
+@Singleton
+public interface TestComponent extends MainComponent{
+
+    @Component.Builder
+    interface Builder extends MainComponent.Builder{
+
+    }
+}
