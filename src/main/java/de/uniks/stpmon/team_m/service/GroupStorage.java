154a5737692c925cdcfaebe7c90b9536cad0249a
Bashar Khoulani <uk083781@student.uni-kassel.de>
2023-05-11 22:14:58 +0200

    feat: Add necessary changes to methods

--- src/main/java/de/uniks/stpmon/team_m/service/GroupStorage.java
+++ src/main/java/de/uniks/stpmon/team_m/service/GroupStorage.java
@@ -2,10 +2,13 @@ package de.uniks.stpmon.team_m.service;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.List;
 
 @Singleton
 public class GroupStorage {
     private String _id;
+    private String name;
+    private List<String> members;
 
     @Inject
     public GroupStorage() {
@@ -18,4 +21,47 @@ public class GroupStorage {
     public void set_id(String _id) {
         this._id = _id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getMembers() {
+        return this.members;
+    }
+
+    public void setMembers(List<String> members) {
+        this.members = members;
+    }
+
+    public void addMember(String newMember) {
+        if (!this.members.contains(newMember)) {
+            this.members.add(newMember);
+        }
+    }
+
+    public void removeMember(String member) {
+        this.members.remove(member);
+    }
+
+    public void clearMembers() {
+        this.members.clear();
+    }
+
+    public boolean hasMember(String member) {
+        return this.members.contains(member);
+    }
+
+    public boolean hasMembers() {
+        return !this.members.isEmpty();
+    }
+
+    public boolean hasNoMembers() {
+        return this.members.isEmpty();
+    }
+
 }
