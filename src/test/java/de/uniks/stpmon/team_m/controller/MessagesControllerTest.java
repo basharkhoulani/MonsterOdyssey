15e4260046e2f8819e1a9f932a0791a312da6ee8
sacharzki <leon.sacharzki@octavia.de>
2023-05-11 14:52:01 +0200

    test(MessageController): add message controller test

--- src/test/java/de/uniks/stpmon/team_m/controller/MessagesControllerTest.java
+++ src/test/java/de/uniks/stpmon/team_m/controller/MessagesControllerTest.java
@@ -1,2 +1,83 @@
-package de.uniks.stpmon.team_m.controller;public class MessagesControllerTest {
+package de.uniks.stpmon.team_m.controller;
+
+import de.uniks.stpmon.team_m.App;
+import de.uniks.stpmon.team_m.Constants;
+import de.uniks.stpmon.team_m.dto.User;
+import de.uniks.stpmon.team_m.rest.UsersApiService;
+import de.uniks.stpmon.team_m.service.UserStorage;
+import de.uniks.stpmon.team_m.service.UsersService;
+import io.reactivex.rxjava3.core.Observable;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class MessagesControllerTest extends ApplicationTest {
+
+    @Mock
+    Provider<UserStorage> userStorageProvider;
+
+    @Mock
+    Provider<MainMenuController> mainMenuControllerProvider;
+
+    @Mock
+    Provider<NewFriendController> newFriendControllerProvider;
+
+    @Mock
+    Provider<GroupController> groupControllerProvider;
+
+    @Mock
+    Provider<UsersService> usersServiceProvider;
+
+    @Mock
+    UsersApiService usersApiService;
+
+    @Spy
+    App app = new App(null);
+
+    @InjectMocks
+    MessagesController messagesController;
+
+
+    @Override
+    public void start(Stage stage) {
+        UserStorage mockUserStorage = Mockito.mock(UserStorage.class);
+        Mockito.when(userStorageProvider.get()).thenReturn(mockUserStorage);
+
+        UsersService mockUsersService = Mockito.mock(UsersService.class);
+        Mockito.when(usersServiceProvider.get()).thenReturn(mockUsersService);
+
+        when(mockUsersService.getUsers(List.of("645cd04c11b590456276e9d9", "645cd086f249626b1eefa92e", "645cd0a34389d5c06620fe64"), null))
+                .thenReturn(Observable.just(List.of(
+                        new User("645cd04c11b590456276e9d9", "Rick", Constants.USER_STATUS_ONLINE, null, null),
+                        new User("645cd086f249626b1eefa92e", "Morty", Constants.USER_STATUS_OFFLINE, null, null),
+                        new User("645cd0a34389d5c06620fe64", "Garbage Goober", Constants.USER_STATUS_OFFLINE, null, null))));
+        when(userStorageProvider.get().getFriends())
+                .thenReturn(List.of("645cd04c11b590456276e9d9", "645cd086f249626b1eefa92e", "645cd0a34389d5c06620fe64"));
+
+        app.start(stage);
+        app.show(messagesController);
+        stage.requestFocus();
+    }
+
+    @Test
+    void displayFriends() throws InterruptedException {
+        clickOn("Morty");
+        Thread.sleep(100000);
+    }
 }
