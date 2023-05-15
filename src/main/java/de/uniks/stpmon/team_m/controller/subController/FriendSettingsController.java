8b80482ac2231474ab233dc65355119e87000fa7
olottko <olottko@gmail.com>
2023-05-15 20:58:49 +0200

    feat: add FriendSettingsController + FriendSettings.fxml

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/FriendSettingsController.java
@@ -0,0 +1,11 @@
+package de.uniks.stpmon.team_m.controller.subController;
+
+import javafx.event.ActionEvent;
+
+public class FriendSettingsController {
+    public void bestFriendAction() {
+    }
+
+    public void deleteFriendAction() {
+    }
+}
