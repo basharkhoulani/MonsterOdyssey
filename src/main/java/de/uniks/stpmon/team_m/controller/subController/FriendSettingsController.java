8f897c3c2fc5d22f9a43d50b962b9e26c3945cea
olottko <olottko@gmail.com>
2023-05-15 21:23:51 +0200

    feat: reduced Warnings

--- src/main/java/de/uniks/stpmon/team_m/controller/subController/FriendSettingsController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/FriendSettingsController.java
@@ -3,10 +3,16 @@ package de.uniks.stpmon.team_m.controller.subController;
 
 import de.uniks.stpmon.team_m.controller.Controller;
 import de.uniks.stpmon.team_m.dto.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class FriendSettingsController extends Controller {
 
     private final User user;
+    @FXML
+    public Button bestFriendButton;
+    @FXML
+    public Button deleteFriendButton;
 
     public FriendSettingsController(User user) {
         this.user = user;
