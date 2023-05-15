8f6c7bf9659fc8904ad2fe17a405ab8e065d2b24
Cheng-JY <chengjiaying.prc@gmail.com>
2023-05-15 19:14:12 +0200

    Update the Code

--- src/test/java/de/uniks/stpmon/team_m/service/UsersServiceTest.java
+++ src/test/java/de/uniks/stpmon/team_m/service/UsersServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
