46eb6533c8862ec90148efc17b2e37d6800b023a
sacharzki <leon.sacharzki@octavia.de>
2023-05-16 18:04:51 +0200

    feat(messageController): add Node creation method for message nodes

--- src/main/java/de/uniks/stpmon/team_m/controller/MessagesController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/MessagesController.java
@@ -1,23 +1,32 @@
 package de.uniks.stpmon.team_m.controller;
 
 import de.uniks.stpmon.team_m.controller.views.UserCell;
+import de.uniks.stpmon.team_m.dto.Message;
 import de.uniks.stpmon.team_m.dto.User;
 import de.uniks.stpmon.team_m.service.GroupStorage;
 import de.uniks.stpmon.team_m.service.UserStorage;
 import de.uniks.stpmon.team_m.service.UsersService;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static de.uniks.stpmon.team_m.Constants.*;
 
@@ -101,11 +110,93 @@ public class MessagesController extends Controller {
         app.show(groupControllerProvider.get());
     }
 
-    public void creteMessageNode() {
-        // TODO:
-        /*
-         * This will probably work similiar to the 'createFriendNode()' method.
-         * */
+    public VBox createMessageNode(Message message) {
+        VBox newMessageNode = new VBox();
+        HBox newMessageValueArea = new HBox();
+        HBox newMessageInfoArea = new HBox();
+
+        newMessageValueArea.maxWidthProperty().bind(chatViewVBox.widthProperty().map(number -> number.intValue() / 2 - 20));
+        newMessageInfoArea.maxWidthProperty().bind(chatViewVBox.widthProperty().map(number -> number.intValue() / 2 - 20));
+
+        boolean userIsSender = message.sender().equals(userStorageProvider.get().get_id());
+        // if user is sender: message is on the right, else on the left
+        newMessageNode.setAlignment(userIsSender ? Pos.TOP_RIGHT : Pos.TOP_LEFT);
+
+        newMessageValueArea.setBorder(new Border(
+                new BorderStroke(
+                        Color.BLACK,
+                        BorderStrokeStyle.SOLID,
+                        new CornerRadii(10),
+                        new BorderWidths(2)
+                )
+        ));
+
+        Text messageText = new Text();
+        messageText.setText(message.body());
+        messageText.wrappingWidthProperty().bind(chatViewVBox.widthProperty().map(number -> number.intValue() / 2 - 20));
+
+        VBox newMessageValueContainer = new VBox();
+        newMessageValueContainer.getChildren().add(messageText);
+        newMessageValueArea.getChildren().add(newMessageValueContainer);
+
+        newMessageValueArea.setPadding(new Insets(2));
+        newMessageValueContainer.setPadding(new Insets(5));
+
+        Label authorAndTime = new Label();
+        if (userIsSender) {
+            authorAndTime.setText(userStorageProvider.get().getName() +
+                    " " +
+                    formatTimeString(message.createdAt()) +
+                    " " +
+                    (message.updatedAt() == null ? "" : "\u270F"));
+
+
+            newMessageValueContainer.setBackground(Background.fill(Paint.valueOf("lightblue")));
+        } else {
+            final String[] senderName = {""};
+            System.out.println(message);
+            usersService.getUser(message.sender()).map(
+                    user -> {
+                        senderName[0] = user.name();
+                        return user;
+                    }
+            ).subscribe().dispose();
+
+            authorAndTime.setText(senderName[0] +
+                    " " +
+                    formatTimeString(message.createdAt()) +
+                    " " +
+                    (message.updatedAt() == null ? "" : "\u270F"));
+        }
+
+        newMessageInfoArea.getChildren().add(authorAndTime);
+
+        Button editButton = new Button("\u270F");
+        editButton.setOnAction(event -> {
+            // @Cheng
+        });
+
+        Button deleteButton = new Button("\uD83D\uDDD1");
+        deleteButton.setOnAction(event -> {
+            // @Cheng
+        });
+        newMessageInfoArea.getChildren().addAll(editButton, deleteButton);
+
+        newMessageInfoArea.setFillHeight(true);
+        HBox.setMargin(editButton, new Insets(0, 0, 0, 5));
+        HBox.setMargin(deleteButton, new Insets(0, 0, 0, 5));
+        editButton.setVisible(false);
+        deleteButton.setVisible(false);
+
+        newMessageNode.getChildren().add(newMessageValueArea);
+        newMessageNode.getChildren().add(newMessageInfoArea);
+
+        newMessageNode.hoverProperty().addListener((observable, oldValue, newValue) -> {
+            editButton.setVisible(newValue);
+            deleteButton.setVisible(newValue);
+        });
+
+        return newMessageNode;
     }
 
     public void sendMessage() {
@@ -116,5 +207,14 @@ public class MessagesController extends Controller {
     public void changeToSettings() {
         groupStorageProvider.get().set_id(LOADING);
         app.show(groupControllerProvider.get());
+    }
+
+    private String formatTimeString(String dateTime) {
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+        System.out.println(dateTime);
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime, inputFormatter);
+
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("HH:mm, dd.MM.yy");
+        return localDateTime.format(outputFormatter);
     }
 }
