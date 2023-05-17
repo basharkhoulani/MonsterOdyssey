c8b1f3cf28a15117443d80cb7ee704e0ca7686de
sacharzki <leon.sacharzki@octavia.de>
2023-05-17 15:31:09 +0200

    refactor(constants): change BUTTON value to unicode

--- src/main/java/de/uniks/stpmon/team_m/Constants.java
+++ src/main/java/de/uniks/stpmon/team_m/Constants.java
@@ -37,7 +37,7 @@ public class Constants {
     public static final String LOGIN_TITLE = "Sign Up & In";
     public static final String PASSWORD_LESS_THAN_8_CHARACTERS = "Password must have at least 8 characters.";
     public static final int PASSWORD_CHARACTER_LIMIT = 8;
-    public static final char BULLET = '●';
+    public static final char BULLET = '\u25cf';
     public static final String REFRESH_TOKEN_PREF = "refreshToken";
     public static final String DELETE_SUCCESS = "Account successfully deleted";
     public static final String CUSTOM_ERROR = "Something went terribly wrong!";
