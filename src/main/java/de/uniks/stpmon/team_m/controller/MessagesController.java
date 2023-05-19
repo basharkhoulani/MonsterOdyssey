7dd7c0f492baee70240eed5dda6a6f72c9bd1826
olottko <olottko@gmail.com>
2023-05-19 11:34:34 +0200

    fix: add Controller for chatviewvbox

--- src/main/java/de/uniks/stpmon/team_m/controller/MessagesController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/MessagesController.java
@@ -2,6 +2,7 @@ package de.uniks.stpmon.team_m.controller;
 
 import de.uniks.stpmon.team_m.Constants;
 import de.uniks.stpmon.team_m.controller.subController.GroupCell;
+import de.uniks.stpmon.team_m.controller.subController.MessagesBoxController;
 import de.uniks.stpmon.team_m.controller.subController.UserCell;
 import de.uniks.stpmon.team_m.dto.Group;
 import de.uniks.stpmon.team_m.dto.Message;
@@ -33,6 +34,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -95,6 +97,7 @@ public class MessagesController extends Controller {
     private ListView<Group> groupListView;
     private final ObservableList<Message> messages = FXCollections.observableArrayList();
     private String chatID;
+    private final List<Controller> subControllers = new ArrayList<>();
 
     @Inject
     public MessagesController() {
@@ -149,241 +152,63 @@ public class MessagesController extends Controller {
 
         userListView.setOnMouseClicked(event -> {
             if (userListView.getSelectionModel().getSelectedItem() != null) {
-                Platform.runLater(() -> openFriendChat("userListView"));
+                MessagesBoxController messagesBoxController = new MessagesBoxController(
+                        messageService,
+                        groupService,
+                        eventListener,
+                        usersService,
+                        groupStorageProvider.get(),
+                        userStorageProvider.get(),
+                        userListView.getSelectionModel().getSelectedItem(),
+                        null,
+                        chatViewVBox,
+                        chatScrollPane,
+                        currentFriendOrGroupText
+                );
+                messagesBoxController.render();
+                subControllers.add(messagesBoxController);
             }
         });
         groupListView.setOnMouseClicked(event -> {
             if (groupListView.getSelectionModel().getSelectedItem() != null) {
-                Platform.runLater(() -> openFriendChat("groupListView"));
+                MessagesBoxController messagesBoxController = new MessagesBoxController(
+                        messageService,
+                        groupService,
+                        eventListener,
+                        usersService,
+                        groupStorageProvider.get(),
+                        userStorageProvider.get(),
+                        null,
+                        groupListView.getSelectionModel().getSelectedItem(),
+                        chatViewVBox,
+                        chatScrollPane,
+                        currentFriendOrGroupText
+                );
+                messagesBoxController.render();
+                subControllers.add(messagesBoxController);
             }
         });
         if (groupStorageProvider.get().get_id() != null) {
-            openFriendChat("other");
+            MessagesBoxController messagesBoxController = new MessagesBoxController(
+                    messageService,
+                    groupService,
+                    eventListener,
+                    usersService,
+                    groupStorageProvider.get(),
+                    userStorageProvider.get(),
+                    userListView.getSelectionModel().getSelectedItem(),
+                    null,
+                    chatViewVBox,
+                    chatScrollPane,
+                    currentFriendOrGroupText
+            );
+            messagesBoxController.render();
+            subControllers.add(messagesBoxController);;
         }
 
         // do something with filledListView
         return parent;
 
-    }
-
-    private void openFriendChat(String origin) {
-
-        final Group chat;
-        if (origin.equals("userListView")) {
-            chat = new Group(null, null, List.of(userListView.getSelectionModel().getSelectedItem()._id(), userStorageProvider.get().get_id()));
-        } else if (origin.equals("groupListView")) {
-            chat = groupListView.getSelectionModel().getSelectedItem();
-        } else {
-            chat = new Group(null, null, List.of(groupStorageProvider.get().get_id(), userStorageProvider.get().get_id()));
-        }
-
-        disposables.add(groupService.getGroups(chat.membersToString())
-                .observeOn(FX_SCHEDULER).subscribe(gotGroups -> {
-                    if (chat.name() == null) {
-                        for (Group group : gotGroups) {
-                            System.out.println("for-loop");
-                            if (group.name() == null) {
-                                System.out.println("group.name() == null");
-                                System.out.println(group._id());
-                                chatID = group._id();
-                                break;
-                            }
-                            chatID = null;
-                        }
-                    } else {
-                        chatID = chat._id();
-                    }
-
-                    if (origin.equals("userListView")) {
-                        this.currentFriendOrGroupText.setText(userListView.getSelectionModel().getSelectedItem().name());
-                    } else if (origin.equals("groupListView")) {
-                        this.currentFriendOrGroupText.setText(chat.name());
-                    } else {
-                        this.currentFriendOrGroupText.setText(groupStorageProvider.get().getName());
-                    }
-                    chatViewVBox.getChildren().clear();
-
-                    System.out.println(chatID);
-                    if (chatID != null) {
-                        groupStorageProvider.get().set_id(chatID);
-
-                        disposables.add(messageService.getGroupMessages(chatID)
-                                .observeOn(FX_SCHEDULER).subscribe(messages -> {
-                                    chatViewVBox.getChildren().clear();
-                                    this.messages.setAll(messages);
-
-                                    for (Message message : messages) {
-                                        chatViewVBox.getChildren().add(this.createMessageNode(message));
-                                    }
-
-                                    chatScrollPane.setVvalue(1.0);
-                                    System.out.println(chatID);
-                                    System.out.println(eventListener.get());
-                                    disposables.add(eventListener.get()
-                                            .listen("groups." + chatID + ".messages.*.*", Message.class)
-                                            .observeOn(FX_SCHEDULER)
-                                            .subscribe(messageEvent -> {
-                                                System.out.println(messageEvent);
-                                                final Message message = messageEvent.data();
-                                                switch (messageEvent.suffix()) {
-                                                    case "created":
-                                                        chatViewVBox.getChildren().add(createMessageNode(message));
-                                                        chatScrollPane.setVvalue(1.0);
-                                                        break;
-                                                    case "updated":
-                                                        for (Node messageNode : chatViewVBox.getChildren()) {
-                                                            if (Objects.equals(messageNode.getId(), message._id())) {
-                                                                VBox messageVBox = (VBox) messageNode;
-                                                                HBox messageValueAreaContainer = (HBox) messageVBox.getChildren().get(0);
-                                                                VBox messageValueArea = (VBox) messageValueAreaContainer.getChildren().get(0);
-                                                                Text messageValueText = (Text) messageValueArea.getChildren().get(0);
-
-                                                                messageValueText.setText(message.body());
-
-                                                                HBox messageInfoArea = (HBox) messageVBox.getChildren().get(1);
-                                                                Label edited = (Label) messageInfoArea.getChildren().get(1);
-                                                                edited.setVisible(true);
-                                                            }
-                                                        }
-                                                        break;
-                                                    case "deleted":
-                                                        chatViewVBox.getChildren().removeIf(
-                                                                node -> Objects.equals(node.getId(), message._id())
-                                                        );
-                                                        break;
-                                                }
-                                            }));
-                                }));
-                    }
-                }));
-
-    }
-
-    public VBox createMessageNode(Message message) {
-        VBox newMessageNode = new VBox();
-        HBox newMessageValueArea = new HBox();
-        HBox newMessageInfoArea = new HBox();
-
-        // @Cheng, so you can call delete and edit functionality on this message
-        newMessageNode.setId(message._id());
-
-        newMessageValueArea.maxWidthProperty().bind(chatViewVBox.widthProperty().map(number -> number.intValue() / 2 - 20));
-        newMessageInfoArea.maxWidthProperty().bind(chatViewVBox.widthProperty().map(number -> number.intValue() / 2 - 20));
-
-        boolean userIsSender = message.sender().equals(userStorageProvider.get().get_id());
-        // if user is sender: message is on the right, else on the left
-        newMessageNode.setAlignment(userIsSender ? Pos.TOP_RIGHT : Pos.TOP_LEFT);
-
-        newMessageValueArea.setBorder(new Border(
-                new BorderStroke(
-                        Color.BLACK,
-                        BorderStrokeStyle.SOLID,
-                        new CornerRadii(10),
-                        new BorderWidths(2)
-                )
-        ));
-
-        Text messageText = new Text();
-        messageText.setText(message.body());
-        messageText.wrappingWidthProperty().bind(chatViewVBox.widthProperty().map(number -> number.intValue() / 2 - 20));
-
-        VBox newMessageValueContainer = new VBox();
-        newMessageValueContainer.getChildren().add(messageText);
-        newMessageValueArea.getChildren().add(newMessageValueContainer);
-
-        newMessageValueArea.setPadding(new Insets(2));
-        newMessageValueContainer.setPadding(new Insets(5));
-
-        Label authorAndTime = new Label();
-        Label edited = new Label();
-        edited.setText("\u270F");
-
-        if (userIsSender) {
-            authorAndTime.setText(userStorageProvider.get().getName() +
-                    " " +
-                    formatTimeString(message.createdAt()) +
-                    " ");
-
-            newMessageValueContainer.setBackground(Background.fill(Paint.valueOf("lightblue")));
-        } else {
-            final String[] senderName = {""};
-            usersService.getUser(message.sender()).map(
-                    user -> {
-                        senderName[0] = user.name();
-                        return user;
-                    }
-            ).subscribe().dispose();
-
-            authorAndTime.setText(senderName[0] +
-                    " " +
-                    formatTimeString(message.createdAt()) +
-                    " ");
-        }
-
-        edited.setVisible(!Objects.equals(message.updatedAt(), message.createdAt()));
-
-        newMessageInfoArea.getChildren().add(authorAndTime);
-        newMessageInfoArea.getChildren().add(edited);
-
-        Button editButton = new Button("\u270F");
-        editButton.setOnAction(event -> {
-            TextInputDialog dialog = new TextInputDialog("");
-            dialog.setTitle("Edit message");
-            dialog.setHeaderText(null);
-            dialog.setContentText("Message:");
-
-            TextArea messageArea = new TextArea();
-            messageArea.setText(message.body());
-            messageArea.setPrefRowCount(4);
-
-            dialog.getDialogPane().setContent(messageArea);
-            Button ok = (Button) dialog.getDialogPane().lookupButton(ButtonType.OK);
-            ok.setOnAction( event1 -> {
-                disposables.add(messageService.updateMessage(chatID, message._id(), messageArea.getText(), MESSAGE_NAMESPACE_GROUPS)
-                        .observeOn(FX_SCHEDULER).subscribe());
-            });
-            dialog.showAndWait();
-        });
-
-        Button deleteButton = new Button("\uD83D\uDDD1");
-        deleteButton.setOnAction(event -> {
-            Alert deleteAlert = new Alert(Alert.AlertType.CONFIRMATION);
-            deleteAlert.setTitle("Confirm delete");
-            deleteAlert.setHeaderText(null);
-            deleteAlert.setContentText("Do you really want to delete this message?");
-
-            deleteAlert.showAndWait().ifPresent(response -> {
-                if (response == ButtonType.OK) {
-                    disposables.add(messageService.deleteMessage(message._id(), chatID, Constants.MESSAGE_NAMESPACE_GROUPS)
-                            .observeOn(FX_SCHEDULER).subscribe());
-                }
-                deleteAlert.close();
-            });
-        });
-        newMessageInfoArea.getChildren().addAll(editButton, deleteButton);
-
-        newMessageInfoArea.setFillHeight(true);
-        HBox.setMargin(editButton, new Insets(0, 0, 0, 5));
-        HBox.setMargin(deleteButton, new Insets(0, 0, 0, 5));
-        editButton.setVisible(false);
-        deleteButton.setVisible(false);
-
-        newMessageNode.getChildren().add(newMessageValueArea);
-        newMessageNode.getChildren().add(newMessageInfoArea);
-
-        newMessageNode.hoverProperty().addListener((observable, oldValue, newValue) -> {
-            editButton.setVisible(newValue);
-            deleteButton.setVisible(newValue);
-        });
-
-        return newMessageNode;
-    }
-
-    private String formatTimeString(String dateTime) {
-        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.parse(dateTime), ZoneId.of("Europe/Berlin"));
-
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("HH:mm, dd.MM.yy");
-        return localDateTime.format(outputFormatter);
     }
 
     public void changeToMainMenu() {
