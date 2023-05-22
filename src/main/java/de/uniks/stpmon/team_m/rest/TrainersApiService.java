7f397ea804adbd74fd4a2ab03b0dd7b67587b5bf
olottko <olottko@gmail.com>
2023-05-22 19:12:44 +0200

    feat: getTrainers

--- src/main/java/de/uniks/stpmon/team_m/rest/TrainersApiService.java
+++ src/main/java/de/uniks/stpmon/team_m/rest/TrainersApiService.java
@@ -3,11 +3,13 @@ package de.uniks.stpmon.team_m.rest;
 import de.uniks.stpmon.team_m.dto.CreateTrainerDto;
 import de.uniks.stpmon.team_m.dto.Trainer;
 import io.reactivex.rxjava3.core.Observable;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
+import retrofit2.http.*;
+
+import java.util.List;
 
 public interface TrainersApiService {
     @POST("regions/{regionId}/trainers")
     Observable<Trainer> create(@Path("regionId") String regionId, @Body CreateTrainerDto createTrainerDto);
+    @GET("regions/{regionsId}/trainers")
+    Observable<List<Trainer>> getTrainers(@Path("regionId") String regionId, @Query("area") String area, @Query("user") String id);
 }
