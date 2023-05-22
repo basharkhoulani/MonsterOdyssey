9ee00e8345828b79d75892760482d595a3a6c558
olottko <olottko@gmail.com>
2023-05-22 21:53:39 +0200

    test: completed

--- src/test/java/de/uniks/stpmon/team_m/service/AreasServiceTest.java
+++ src/test/java/de/uniks/stpmon/team_m/service/AreasServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -52,6 +53,26 @@ public class AreasServiceTest {
 
     @Test
     void getAreaTest() {
+        when(areasApiService.getArea("646bc436cfee07c0e408466f", "646bc6960b9bca2eae935a69")).thenReturn(Observable.just(
+                new Area(
+                        "2023-05-22T17:51:46.772Z",
+                        "2023-05-22T17:51:46.772Z",
+                        "646bc3c0a9ac1b375fb41d93",
+                        "646bc436cfee07c0e408466f",
+                        "Albertina",
+                        new Map()
+                )
+        ));
 
+        final Area area = areasService.getArea("646bc436cfee07c0e408466f", "646bc6960b9bca2eae935a69").blockingFirst();
+
+        assertEquals("2023-05-22T17:51:46.772Z", area.createdAt());
+        assertEquals("2023-05-22T17:51:46.772Z", area.updatedAt());
+        assertEquals("646bc3c0a9ac1b375fb41d93", area._id());
+        assertEquals("646bc436cfee07c0e408466f", area.region());
+        assertEquals("Albertina", area.name());
+        assertNotNull(area.map());
+
+        verify(areasApiService).getArea("646bc436cfee07c0e408466f", "646bc6960b9bca2eae935a69");
     }
 }
