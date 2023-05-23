928b1179d0730b9e77e63aa57f1b22f65d5447d2
olottko <olottko@gmail.com>
2023-05-23 21:43:22 +0200

    feat: add RegionServiceTest and changed old test so everything works

--- src/main/java/de/uniks/stpmon/team_m/dto/Region.java
+++ src/main/java/de/uniks/stpmon/team_m/dto/Region.java
@@ -1,13 +1,11 @@
 package de.uniks.stpmon.team_m.dto;
 
-import org.mapeditor.core.Map;
-
 public record Region(
         String createdAt,
         String updatedAt,
         String _id,
         String name,
         Spawn spawn,
-        Map map
+        Object map
 ) {
 }
