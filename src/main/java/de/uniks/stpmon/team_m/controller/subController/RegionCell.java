7ff87924caa419e5139b11965c334bf01fb38032
Bashar Khoulani <uk083781@student.uni-kassel.de>
2023-05-24 17:12:50 +0200

    feat: Refactor RegionCell

--- src/main/java/de/uniks/stpmon/team_m/controller/subController/RegionCell.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/RegionCell.java
@@ -10,9 +10,22 @@ public class RegionCell extends ListCell<Region> {
 
     private final ToggleGroup regionToggleGroup;
 
+    /**
+     * RegionCell is used to handle the region cells in Main Menu.
+     * Every region cell has a radio button with the name of the region.
+     * The region cell has a regionToggleGroup which contains all region cells.
+     * The radio button has the region as user data.
+     *
+     * @param regionToggleGroup The {@link ToggleGroup} is used to handle the region cells.
+     */
+
     public RegionCell(ToggleGroup regionToggleGroup) {
         this.regionToggleGroup = regionToggleGroup;
     }
+
+    /**
+     * The updateItem method is used to update the region cell.
+     */
 
     @Override
     protected void updateItem(Region item, boolean empty) {
