ef009d3f9919cc50ecb331de9a74f7289d9b32b3
XadorXyves <uk070539@student.uni-kassel.de>
2023-05-24 14:57:55 +0200

    feat: Avatar Selection Dialog, new fxml, new AvatarSelectionController and AvatarSelectionControllerTest (Empty)

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/AvatarSelectionController.java
@@ -0,0 +1,55 @@
+package de.uniks.stpmon.team_m.controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+
+public class AvatarSelectionController extends Controller {
+
+    @FXML
+    public Label uploadErrorLabel;
+    @FXML
+    public ImageView avatar1ImageView;
+    @FXML
+    public RadioButton avatar1RadioButton;
+    @FXML
+    public ImageView avatar2ImageView;
+    @FXML
+    public RadioButton avatar2RadioButton;
+    @FXML
+    public ImageView avatar3ImageView;
+    @FXML
+    public RadioButton avatar3RadioButton;
+    @FXML
+    public ImageView avatar4ImageView;
+    @FXML
+    public RadioButton avatar4RadioButton;
+    @FXML
+    public ToggleGroup selectAvatar;
+
+
+    @Override
+    public Parent render() {
+        return super.render();
+    }
+
+    public void selectAvatar1() {
+
+    }
+
+    public void selectAvatar2() {
+
+    }
+
+    public void selectAvatar3() {
+
+    }
+
+    public void selectAvatar4() {
+
+    }
+}
---
+++ src/test/java/de/uniks/stpmon/team_m/controller/AvatarSelectionControllerTest.java
@@ -0,0 +1,9 @@
+package de.uniks.stpmon.team_m.controller;
+
+
+import org.testfx.framework.junit5.ApplicationTest;
+
+//TODO
+public class AvatarSelectionControllerTest extends ApplicationTest {
+
+}
