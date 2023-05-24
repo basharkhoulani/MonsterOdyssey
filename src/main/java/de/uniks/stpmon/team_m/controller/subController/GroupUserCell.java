b4f069d0d069896841bad6302133deb1671b98a0
Bashar Khoulani <uk083781@student.uni-kassel.de>
2023-05-24 13:19:03 +0200

    feat: Refactor GroupUserCell

--- src/main/java/de/uniks/stpmon/team_m/controller/subController/GroupUserCell.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/GroupUserCell.java
@@ -21,6 +21,22 @@ public class GroupUserCell extends UserCell {
     private final ObservableList<User> chosenUsers;
     private final List<User> friends;
 
+    /**
+     * GroupUserCell is used to handle the group user cells in the GroupController.
+     * It includes a button to remove and add to the group.
+     * GroupUserCell extends UserCell which includes the status, best friend status, and name of the user.
+     *
+     * @param preferences       The preferences of the user.
+     *                                  The preferences are used to get the best friend status of the user.
+     *
+     * @param chosenUsers       The chosen users of the group.
+     *                                  The chosen users are used to remove and add users to the group.
+     *
+     * @param friendsListView   The friends list view.
+     * @param foreignListView   The foreign list view.
+     * @param friends           The friends of the user.
+     */
+
     public GroupUserCell(Preferences preferences, ObservableList<User> chosenUsers, ListView<User> friendsListView,
                          ListView<User> foreignListView, List<User> friends) {
         super(preferences);
@@ -29,6 +45,11 @@ public class GroupUserCell extends UserCell {
         this.foreignListView = foreignListView;
         this.friends = friends;
     }
+
+    /**
+     * Updates and renders the group user cell.
+     * It subdivides the chosen users into friends and foreign users.
+     */
 
     @Override
     protected void updateItem(User item, boolean empty) {
@@ -53,28 +74,55 @@ public class GroupUserCell extends UserCell {
         }
     }
 
+
+    /**
+     * Adds or removes the user to the group.
+     * If the user is already in the group, the user will be removed from the group.
+     * If the user is not in the group, the user will be added to the group.
+     * The user will be added to the friends list view if the user is a friend.
+     * The user will be added to the foreign list view if the user is not a friend.
+     *
+     * @param item              The user.
+     * @param addOrRemoveButton The button to add or remove the user to the group.
+     */
+
     private void addOrRemoveToGroup(User item, Button addOrRemoveButton) {
         if (chosenUsers.contains(item)) {
-            chosenUsers.remove(item);
-            addOrRemoveButton.setText(ADD_MARK);
-            if (friends.contains(item)) {
-                friendsListView.getItems().remove(item);
-                addUserAndSort(friendsListView, item);
-            } else {
-                foreignListView.getItems().remove(item);
-            }
+            removeUser(item, addOrRemoveButton);
         } else {
-            chosenUsers.add(item);
-            addOrRemoveButton.setText(CHECK_MARK);
-            if (friends.contains(item)) {
-                friendsListView.getItems().remove(item);
-                addUserAndSort(friendsListView, item);
-            } else {
-                foreignListView.getItems().remove(item);
-                addUserAndSort(foreignListView, item);
-            }
+            addUser(item, addOrRemoveButton);
         }
     }
+
+    private void addUser(User item, Button addOrRemoveButton) {
+        chosenUsers.add(item);
+        addOrRemoveButton.setText(CHECK_MARK);
+        if (friends.contains(item)) {
+            friendsListView.getItems().remove(item);
+            addUserAndSort(friendsListView, item);
+        } else {
+            foreignListView.getItems().remove(item);
+            addUserAndSort(foreignListView, item);
+        }
+    }
+
+    private void removeUser(User item, Button addOrRemoveButton) {
+        chosenUsers.remove(item);
+        addOrRemoveButton.setText(ADD_MARK);
+        if (friends.contains(item)) {
+            friendsListView.getItems().remove(item);
+            addUserAndSort(friendsListView, item);
+        } else {
+            foreignListView.getItems().remove(item);
+        }
+    }
+
+    /**
+     * Adds the user to the list view and sorts the list view.
+     *
+     * @param listView The list view.
+     * @param item     The user.
+     */
 
     private void addUserAndSort(ListView<User> listView, User item) {
         listView.getItems().add(item);
