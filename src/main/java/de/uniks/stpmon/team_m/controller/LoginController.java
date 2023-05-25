9f83e031a36598444ebd2862de6777b6cb068c73
basharkhoulani <uk083781@uni-kassel.de>
2023-05-25 15:37:30 +0200

    feat: Refactor LoginController

--- src/main/java/de/uniks/stpmon/team_m/controller/LoginController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/LoginController.java
@@ -41,19 +41,6 @@ public class LoginController extends Controller {
     public Label welcomeLabel;
     @FXML
     public Label gameNameLabel;
-
-    private PasswordFieldSkin skin;
-
-    private BooleanBinding isInvalidUsername;
-    private BooleanBinding isInvalidPassword;
-
-
-    private final SimpleStringProperty username = new SimpleStringProperty();
-    private final SimpleStringProperty password = new SimpleStringProperty();
-    private final SimpleBooleanProperty rememberMe = new SimpleBooleanProperty();
-    private String information = EMPTY_STRING;
-
-
     @Inject
     Provider<MainMenuController> mainMenuControllerProvider;
     @Inject
@@ -62,16 +49,34 @@ public class LoginController extends Controller {
     AuthenticationService authenticationService;
     @Inject
     UsersService usersService;
+    private PasswordFieldSkin skin;
+    private final SimpleStringProperty username = new SimpleStringProperty();
+    private final SimpleStringProperty password = new SimpleStringProperty();
+    private final SimpleBooleanProperty rememberMe = new SimpleBooleanProperty();
+    private String information;
 
+    /**
+     * LoginController is used to show the login screen and to login or signup the user.
+     */
 
     @Inject
     public LoginController() {
     }
 
+    /**
+     * This method is used to set the title of the login screen.
+     */
+
     @Override
     public String getTitle() {
         return LOGIN_TITLE;
     }
+
+    /**
+     * This method is used to render JavaFX elements of the login screen.
+     *
+     * @return Parent object
+     */
 
     @Override
     public Parent render() {
@@ -84,8 +89,8 @@ public class LoginController extends Controller {
         passwordField.textProperty().bindBidirectional(password);
         rememberMeCheckbox.selectedProperty().bindBidirectional(rememberMe);
 
-        isInvalidUsername = username.isEmpty();
-        isInvalidPassword = password.length().lessThan(PASSWORD_CHARACTER_LIMIT);
+        BooleanBinding isInvalidUsername = username.isEmpty();
+        BooleanBinding isInvalidPassword = password.length().lessThan(PASSWORD_CHARACTER_LIMIT);
         signInButton.disableProperty().bind(isInvalidPassword.or(isInvalidUsername));
         signUpButton.disableProperty().bind(isInvalidPassword.or(isInvalidUsername));
 
@@ -96,48 +101,63 @@ public class LoginController extends Controller {
         return parent;
     }
 
+    /**
+     * This method is used to log in the user and update the status to online.
+     * It also shows the main menu screen. It handles the error if the user is not found.
+     */
+
     public void signIn() {
         passwordErrorLabel.setText(EMPTY_STRING);
-
-        if (isInvalidPassword.or(isInvalidUsername).get()) {
-            return;
-        }
-
-        disposables.add(authenticationService
-                .login(username.get(), password.get(), rememberMe.get())
-                .observeOn(FX_SCHEDULER)
-                .subscribe(loginResult -> {
+        disposables.add(authenticationService.login(username.get(), password.get(), rememberMe.get())
+                .observeOn(FX_SCHEDULER).subscribe(loginResult -> {
                     userStatusUpdate(USER_STATUS_ONLINE);
                     app.show(mainMenuControllerProvider.get());
                 }, error -> errorHandle(error.getMessage())));
     }
 
+    /**
+     * This method is used to sign up the user and show the main menu screen.
+     * It handles the error if the username is already taken.
+     */
+
     public void signUp() {
         passwordErrorLabel.setText(EMPTY_STRING);
-
-        if (isInvalidPassword.or(isInvalidUsername).get()) {
-            return;
-        }
-
-        disposables.add(usersService
-                .createUser(username.get(), null, password.get())
-                .observeOn(FX_SCHEDULER)
-                .subscribe(userResult -> signIn(),
-                        error -> errorHandle(error.getMessage())));
+        disposables.add(usersService.createUser(username.get(), null, password.get())
+                .observeOn(FX_SCHEDULER).subscribe(userResult -> signIn(), error -> errorHandle(error.getMessage())));
     }
+
+    /**
+     * This method is used to show the password of the user in the PasswordField.
+     */
 
     public void showPassword() {
         skin.setMask(skin.getNotMask());
         passwordField.setText(passwordField.getText());
     }
 
+    /**
+     * This method is used to show a message when the user has been deleted from AccountSettingController
+     */
+
     public void showInformation() {
         informationLabel.setText(this.information);
     }
 
+    /**
+     * This method is used to set the information if the user has been deleted from AccountSettingController
+     *
+     * @param information String, the success message
+     */
+
     public void setInformation(String information) {
         this.information = information;
     }
+
+    /**
+     * This method is used to handle the error messages.
+     *
+     * @param error String, the error message
+     */
 
     public void errorHandle(String error) {
         if (error.contains(HTTP_401)) {
@@ -148,6 +168,12 @@ public class LoginController extends Controller {
             passwordErrorLabel.setText(CUSTOM_ERROR);
         }
     }
+
+    /**
+     * This method is used to update the status of the user.
+     *
+     * @param status String, the status of the user
+     */
 
     public void userStatusUpdate(String status) {
         if (userStorage.get().get_id() != null) {
