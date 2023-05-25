bbc8ecdf94e60f12e6631ffa422a6a1d633bfeec
basharkhoulani <uk083781@uni-kassel.de>
2023-05-25 11:21:21 +0200

    feat: Refactor UserCell

--- src/main/java/de/uniks/stpmon/team_m/controller/subController/UserCell.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/UserCell.java
@@ -18,11 +18,28 @@ import static javafx.geometry.Pos.CENTER;
 public class UserCell extends ListCell<User> {
     Preferences preferences;
 
+    /**
+     * UserCell is used to handle user cells in every ListView. It is the parent class for every user cell.
+     * It includes the status of the user, status as best friend and the username.
+     * rootHBox is the root of the user cell which contains data about the user.
+     *
+     * @param preferences The {@link Preferences} of the user are used to get the best friend status of the user.
+     *                    If the user is a best friend, a star will be shown next to the username, and if the user is
+     *                    not a best friend, a circle will be shown next to the username.
+     */
+
     public UserCell(Preferences preferences) {
         this.preferences = preferences;
     }
 
     private HBox rootHBox;
+
+    /**
+     * Updates the user cell if changes are made.
+     *
+     * @param user  The selected {@link User}
+     * @param empty The empty status of the user cell
+     */
 
     @Override
     protected void updateItem(User user, boolean empty) {
@@ -55,6 +72,13 @@ public class UserCell extends ListCell<User> {
             setText(null);
         }
     }
+
+
+    /**
+     * Returns the root of the user cell.
+     *
+     * @return The root of the user cell
+     */
 
     public HBox getRootHBox() {
         return rootHBox;
