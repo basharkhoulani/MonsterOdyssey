99254e68576537a913ca471bf1c73df0df7e808d
Harun Dalkiran <harundlkrn46@gmail.com>
2023-05-28 09:28:58 +0200

    feat: add attribute to Region

--- src/main/java/de/uniks/stpmon/team_m/dto/Region.java
+++ src/main/java/de/uniks/stpmon/team_m/dto/Region.java
@@ -1,7 +1,15 @@
 package de.uniks.stpmon.team_m.dto;
 
+import netscape.javascript.JSObject;
+
+import javax.json.JsonObject;
+
 public record Region(
         String _id,
-        String name
+        String name,
+        String createdAt,
+        String updatedAt,
+        JsonObject map,
+        Spawn spawn
 ) {
 }
