4e723dce394a25d4835003c667e1a33568efbf22
olottko <olottko@gmail.com>
2023-06-01 20:42:26 +0200

    test: add test dummies

---
+++ src/test/java/de/uniks/stpmon/team_m/service/PresetsServiceTest.java
@@ -0,0 +1,56 @@
+package de.uniks.stpmon.team_m.service;
+
+import de.uniks.stpmon.team_m.rest.PresetsApiService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PresetsServiceTest {
+    @Mock
+    PresetsApiService presetsApiService;
+    @InjectMocks
+    PresetsService presetsService;
+
+    @Test
+    void getTilesetTest() {
+
+    }
+
+    @Test
+    void getCharactersTest() {
+
+    }
+
+    @Test
+    void getCharacterTest() {
+
+    }
+
+    @Test
+    void getMonstersTest() {
+
+    }
+
+    @Test
+    void getMonsterTest() {
+
+    }
+
+    @Test
+    void getMonsterImageTest() {
+
+    }
+
+    @Test
+    void getAbilitiesTest() {
+
+    }
+
+    @Test
+    void getAbilityTest() {
+
+    }
+}
