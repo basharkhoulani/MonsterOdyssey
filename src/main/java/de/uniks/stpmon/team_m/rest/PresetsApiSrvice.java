458631fc62d1b565a69647e480a0d0e1c6ff7371
olottko <olottko@gmail.com>
2023-06-01 20:24:39 +0200

    feat: add all calls

--- src/main/java/de/uniks/stpmon/team_m/rest/PresetsApiSrvice.java
+++ src/main/java/de/uniks/stpmon/team_m/rest/PresetsApiSrvice.java
@@ -1,5 +1,7 @@
 package de.uniks.stpmon.team_m.rest;
 
+import de.uniks.stpmon.team_m.dto.AbilityDto;
+import de.uniks.stpmon.team_m.dto.MonsterTypeDto;
 import io.reactivex.rxjava3.core.Observable;
 import okhttp3.ResponseBody;
 import retrofit2.http.GET;
@@ -9,8 +11,20 @@ import java.io.ByteArrayInputStream;
 import java.util.List;
 
 public interface PresetsApiSrvice {
+    @GET("presets/tilesets/{filename}")
+    Observable<ResponseBody> getTileset(@Path("filename") String filename);
     @GET("presets/characters")
     Observable<List<String>> getCharacters();
     @GET("presets/characters/{filename}")
     Observable<ResponseBody> getCharacter(@Path("filename") String filename);
+    @GET("presets/monsters")
+    Observable<List<MonsterTypeDto>> getMonsters();
+    @GET("presets/monsters/{id}")
+    Observable<MonsterTypeDto> getMonster(@Path("id") String id);
+    @GET("presets/monsters/{id}/image")
+    Observable<ResponseBody> getMonsterImage(@Path("id") String id);
+    @GET("presets/abilities")
+    Observable<List<AbilityDto>> getAbilities();
+    @GET("presets/abilities/{id}")
+    Observable<AbilityDto> getAbility(@Path("id") String id);
 }
