19f2c8ea8581e9a48d0e374292d7a67c3c10f72a
Fin <uk059783@student.uni-kassel.de>
2023-06-02 08:08:10 +0200

    feat: Added Monsters- and Settingsbutton to Ingame, also added a function to show the TrainerSettings

--- src/main/java/de/uniks/stpmon/team_m/controller/IngameController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/IngameController.java
@@ -1,14 +1,19 @@
 package de.uniks.stpmon.team_m.controller;
 
 
+import de.uniks.stpmon.team_m.Main;
+import de.uniks.stpmon.team_m.controller.subController.IngameTrainerSettingsController;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.DialogPane;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.skin.ButtonBarSkin;
 import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.Window;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -20,8 +25,18 @@ public class IngameController extends Controller {
 
     @FXML
     public Button helpSymbol;
+    @FXML
+    public Button monstersButton;
+    @FXML
+    public Button settingsButton;
+
+    private IngameTrainerSettingsController trainerSettingsController;
+
     @Inject
     Provider<MainMenuController> mainMenuControllerProvider;
+
+    @Inject
+    Provider<IngameTrainerSettingsController> ingameTrainerSettingsControllerProvider;
 
     /**
      * IngameController is used to show the In-Game screen and to pause the game.
@@ -29,6 +44,13 @@ public class IngameController extends Controller {
 
     @Inject
     public IngameController() {
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        trainerSettingsController = ingameTrainerSettingsControllerProvider.get();
+        trainerSettingsController.init();
     }
 
     /**
@@ -117,5 +139,17 @@ public class IngameController extends Controller {
             app.getStage().getScene().setOnKeyPressed(null);
             app.show(mainMenuControllerProvider.get());
         }
+    }
+
+
+    public void showTrainerSettings() {
+        Dialog<?> trainerSettingsDialog = new Dialog<>();
+        trainerSettingsDialog.setTitle("Trainer Profil");
+        trainerSettingsDialog.getDialogPane().setContent(trainerSettingsController.render());
+        Window popUp = trainerSettingsDialog.getDialogPane().getScene().getWindow();
+        popUp.setOnCloseRequest(evt ->
+            ((Stage) trainerSettingsDialog.getDialogPane().getScene().getWindow()).close()
+        );
+        trainerSettingsDialog.showAndWait();
     }
 }
