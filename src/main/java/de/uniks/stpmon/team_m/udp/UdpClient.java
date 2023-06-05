38486f034464c2d1a3e27282804277beaddf6885
olottko <olottko@gmail.com>
2023-06-05 18:07:56 +0200

    feat: add UdpEventListener and UdpClient

---
+++ src/main/java/de/uniks/stpmon/team_m/udp/UdpClient.java
@@ -0,0 +1,97 @@
+package de.uniks.stpmon.team_m.udp;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+    public class UdpClient {
+        private final InetAddress serverAddress;
+        private final int serverPort;
+        private final List<Consumer<String>> messageHandlers = Collections.synchronizedList(new ArrayList<>());
+        private DatagramSocket socket;
+
+        public UdpClient(InetAddress serverAddress, int serverPort) {
+            this.serverAddress = serverAddress;
+            this.serverPort = serverPort;
+        }
+
+        public boolean isOpen() {
+            return this.socket != null && !this.socket.isClosed();
+        }
+
+        public void open() throws IOException {
+            socket = new DatagramSocket();
+        }
+
+        public void close() {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+        }
+
+        @OnOpen
+        public void onOpen(DatagramSocket socket) {
+            this.socket = socket;
+        }
+
+        @OnClose
+        public void onClose(DatagramSocket socket) {
+            this.socket = null;
+        }
+
+         @OnMessage
+        public void onMessage(String message) {
+            for (final Consumer<String> handler : this.messageHandlers) {
+                handler.accept(message);
+            }
+        }
+
+        @OnError
+        public void onError(Throwable error) {
+            error.printStackTrace();
+        }
+
+        public void addMessageHandler(Consumer<String> msgHandler) {
+            this.messageHandlers.add(msgHandler);
+        }
+
+        public void removeMessageHandler(Consumer<String> msgHandler) {
+            this.messageHandlers.remove(msgHandler);
+        }
+
+        public void sendMessage(String message) throws IOException {
+            byte[] sendData = message.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+            socket.send(sendPacket);
+        }
+
+        protected void startReceiving() {
+            new Thread(() -> {
+                while (isOpen()) {
+                    try {
+                        byte[] receiveData = new byte[1024];
+                        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                        socket.receive(receivePacket);
+                        String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                        onMessage(message);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+
+        public boolean hasMessageHandler() {
+            return !this.messageHandlers.isEmpty();
+        }
+    }
+
---
+++ src/main/java/de/uniks/stpmon/team_m/udp/UdpEventListener.java
@@ -0,0 +1,102 @@
+package de.uniks.stpmon.team_m.udp;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.uniks.stpmon.team_m.dto.Event;
+import de.uniks.stpmon.team_m.utils.TokenStorage;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.regex.Pattern;
+import java.util.function.Consumer;
+
+import static de.uniks.stpmon.team_m.Constants.SERVER_PORT;
+import static de.uniks.stpmon.team_m.Constants.UDP_URL;
+
+@Singleton
+public class UdpEventListener {
+    private final TokenStorage tokenStorage;
+    private final ObjectMapper mapper;
+    private UdpClient client;
+
+    @Inject
+    public UdpEventListener(TokenStorage tokenStorage, ObjectMapper mapper) {
+        this.tokenStorage = tokenStorage;
+        this.mapper = mapper;
+    }
+
+    private void ensureOpen() {
+        if (client != null && client.isOpen()) {
+            return;
+        }
+        try {
+            InetAddress address = InetAddress.getByName(UDP_URL);
+            client = new UdpClient(address, SERVER_PORT);
+            client.open();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> Observable<Event<T>> listen(String pattern, Class<T> type) {
+        return Observable.create(emitter -> {
+            this.ensureOpen();
+            send(new Event<>("subscribe", pattern));
+            final Consumer<String> handler = createPatternHandler(mapper, pattern, type, emitter);
+            client.addMessageHandler(handler);
+            emitter.setCancellable(() -> removeEventHandler(pattern, handler));
+        });
+    }
+
+    public void send(Object data) {
+        try {
+            String message = mapper.writeValueAsString(data);
+            client.sendMessage(message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> Consumer<String> createPatternHandler(ObjectMapper mapper, String pattern, Class<T> type, ObservableEmitter<Event<T>> emitter) {
+        return eventStr -> {
+            final Pattern regex = Pattern.compile(pattern.replace(".", "\\.").replace("*", "[^.]*"));
+            try {
+                final JsonNode node = mapper.readTree(eventStr);
+                final String event = node.get("event").asText();
+                if (!regex.matcher(event).matches()) {
+                    return;
+                }
+
+                final T data = mapper.treeToValue(node.get("data"), type);
+                emitter.onNext(new Event<>(event, data));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    private void removeEventHandler(String pattern, Consumer<String> handler) {
+        if (client == null) {
+            return;
+        }
+
+        send(new Event<>("unsubscribe", pattern));
+        client.removeMessageHandler(handler);
+        if (!client.hasMessageHandler()) {
+            close();
+        }
+    }
+
+    private void close() {
+        if (client != null) {
+            client.close();
+            client = null;
+        }
+    }
+}
