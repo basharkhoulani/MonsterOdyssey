298ef573caf171a0ee202da4094c3b5029140a84
Harun Dalkiran <harundlkrn46@gmail.com>
2023-06-15 22:44:36 +0200

    feat: add minimap

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/IngameMiniMapController.java
@@ -0,0 +1,10 @@
+package de.uniks.stpmon.team_m.controller.subController;
+
+import de.uniks.stpmon.team_m.controller.Controller;
+import javafx.scene.Parent;
+
+public class IngameMiniMapController extends Controller {
+    public Parent render() {
+        return super.render();
+    }
+}
