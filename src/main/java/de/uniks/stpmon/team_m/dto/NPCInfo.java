2754bcdcd558c950eaa648b81381b13243a2105e
Cheng-JY <chengjiaying.prc@gmail.com>
2023-06-20 20:10:43 +0200

    Feat: Update pervious existed Dtos

--- src/main/java/de/uniks/stpmon/team_m/dto/NPCInfo.java
+++ src/main/java/de/uniks/stpmon/team_m/dto/NPCInfo.java
@@ -1,6 +1,13 @@
 package de.uniks.stpmon.team_m.dto;
 
+import java.util.List;
+
 public record NPCInfo(
-        boolean walkRandomly
+        boolean walkRandomly,
+        boolean encounterOnSight,
+        boolean encounterOnTalk,
+        boolean canHeal,
+        List<String> encountered,
+        List<String> starters
 ) {
 }
