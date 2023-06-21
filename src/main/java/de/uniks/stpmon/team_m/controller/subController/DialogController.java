5ac0a5d97da462400d555d12a809dc180e2f3c93
sacharzki <leon.sacharzki@octavia.de>
2023-06-21 15:48:04 +0200

    feat(DialogController): extend controller functionality

--- src/main/java/de/uniks/stpmon/team_m/controller/subController/DialogController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/DialogController.java
@@ -3,6 +3,8 @@ package de.uniks.stpmon.team_m.controller.subController;
 import de.uniks.stpmon.team_m.controller.Controller;
 import de.uniks.stpmon.team_m.dto.Trainer;
 import de.uniks.stpmon.team_m.utils.TrainerStorage;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -11,10 +13,29 @@ public class DialogController extends Controller {
     @Inject
     Provider<TrainerStorage> trainerStorageProvider;
 
-    private final String npcName;
+    private final String[] npcTexts;
+    private int currentTextIndex;
+    private final Text currentText;
+    private final int amountOfTexts;
 
     @Inject
-    public DialogController(Trainer npc) {
-        npcName = npc.name();
+    public DialogController(Trainer npc, TextFlow dialogTextFlow) {
+        NpcTextManager npcTextManager = NpcTextManager.getInstance();
+        this.npcTexts = npcTextManager.getNpcTexts(npc._id());
+        this.currentTextIndex = 0;
+        this.currentText = new Text(npcTexts[currentTextIndex]);
+        this.amountOfTexts = npcTexts.length;
+
+        dialogTextFlow.getChildren().add(currentText);
+    }
+
+
+    public boolean continueDialog() {
+        if (++currentTextIndex == amountOfTexts) {
+            return false;
+        } else {
+            this.currentText.setText(npcTexts[currentTextIndex]);
+            return true;
+        }
     }
 }
