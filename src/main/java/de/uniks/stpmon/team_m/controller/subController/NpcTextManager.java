9f3aabb4301502f06a9d40eb18aaa2b2b99aee93
sacharzki <leon.sacharzki@octavia.de>
2023-06-21 16:37:16 +0200

    refactor(npcTextManager): add bit of test data

--- src/main/java/de/uniks/stpmon/team_m/controller/subController/NpcTextManager.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/NpcTextManager.java
@@ -13,7 +13,14 @@ public class NpcTextManager {
 //        String[] defaultTexts = {"%NPC_DEFAULT0", "%NPC_DEFAULT1","%NPC_DEFAULT2"};
         String[] defaultTexts = {"Hey there, partner!", "How's your adventure going so far?", "I'm having a blast!"};
 
+        String[] ProfAlbert = {"Hello young fella! For the beginning of your adventure, I want to give you a partner.",
+                                "Please choose between these three cute, little bois:"};
+        String[] ProfAlbertAlreadyEncountered = {"Go into the wide, open world and explore to your hearts content!"};
+
         npcTexts.put("Default", defaultTexts);
+
+        npcTexts.put("645e32c6866ace359554a802", ProfAlbert);
+        npcTexts.put("645e32c6866ace359554a802alreadyEncountered", ProfAlbertAlreadyEncountered);
     }
 
     public static NpcTextManager getInstance() {
