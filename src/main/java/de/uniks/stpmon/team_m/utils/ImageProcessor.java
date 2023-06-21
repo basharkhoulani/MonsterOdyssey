5f97032da546abb0ce4abf958c70fe3902720505
XadorXyves <uk070539@student.uni-kassel.de>
2023-06-21 15:19:42 +0200

    feat: character view scaling funktion in ImageProcessor fix: character selection now showing scaled character image

--- src/main/java/de/uniks/stpmon/team_m/utils/ImageProcessor.java
+++ src/main/java/de/uniks/stpmon/team_m/utils/ImageProcessor.java
@@ -9,10 +9,13 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.net.URISyntaxException;
 import java.util.Base64;
 import java.util.Objects;
+
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
+
 import java.io.InputStream;
 
 import static de.uniks.stpmon.team_m.Constants.*;
@@ -33,14 +36,14 @@ public class ImageProcessor {
             image = new javafx.scene.image.Image(Objects.requireNonNull(App.class.getResource(AVATAR_1)).toString());
             return image;
         }
-        if (avatar.startsWith("data:image/png;base64, ")){
+        if (avatar.startsWith("data:image/png;base64, ")) {
             byte[] imageBytes = Base64.getDecoder().decode(avatar.replaceFirst("data:image/png;base64, ", ""));
             ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
             image = new javafx.scene.image.Image(bis);
-        } else if (avatar.startsWith("data:image/jpg;base64, ")){
-                byte[] imageBytes = Base64.getDecoder().decode(avatar.replaceFirst("data:image/jpg;base64, ", ""));
-                ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
-                image = new javafx.scene.image.Image(bis);
+        } else if (avatar.startsWith("data:image/jpg;base64, ")) {
+            byte[] imageBytes = Base64.getDecoder().decode(avatar.replaceFirst("data:image/jpg;base64, ", ""));
+            ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
+            image = new javafx.scene.image.Image(bis);
         } else {
             image = new javafx.scene.image.Image(Objects.requireNonNull(App.class.getResource(AVATAR_2)).toString());
         }
@@ -76,29 +79,30 @@ public class ImageProcessor {
 
         return bufferedImage;
     }
+
     /**
      * This method is used to crop the
-     * @param trainerChumk into 6 Images, that displays the player character moving in some direction
-     * @param direction : either 0 [up], 1 [right], 2 [down], or 3 [left] are possible directions
-     * @param isWalking : determine if the character is moving or not
+     *
+     * @param trainerChunk into 6 Images, that displays the player character moving in some direction
+     * @param direction    : either 0 [up], 1 [right], 2 [down], or 3 [left] are possible directions
+     * @param isWalking    : determine if the character is moving or not
      */
-    public static Image[] cropTrainerImages(Image trainerChumk, int direction, Boolean isWalking) {
+    public static Image[] cropTrainerImages(Image trainerChunk, int direction, Boolean isWalking) {
         Image[] array = new Image[6];
-        int x,y;
+        int x, y;
         if (isWalking) {
             y = 71;
-        }
-        else {
+        } else {
             y = 39;
         }
         switch (direction) {
-            case 1    -> x = 0;
-            case 0    -> x = 96;
-            case 3    -> x = 192;
-            default   -> x = 288;
+            case 1 -> x = 0;
+            case 0 -> x = 96;
+            case 3 -> x = 192;
+            default -> x = 288;
         }
-        for (int i=0; i < 6; i++) {
-            array[i] = getSubImage(trainerChumk, x, y, 16, 25);
+        for (int i = 0; i < 6; i++) {
+            array[i] = getSubImage(trainerChunk, x, y, 16, 25);
             x += 16;
         }
         return array;
@@ -112,6 +116,30 @@ public class ImageProcessor {
         return null;
     }
 
+    /**
+     * This method is used to crop the
+     *
+     * @param premadeCharacter for instance "Premade_Character_01.png", also found in Constants
+     */
+    public static WritableImage showScaledFrontCharacter(String premadeCharacter) {
+        try {
+            File imageFile = new File(Objects.requireNonNull(App.class.getResource("charactermodels/" + premadeCharacter)).toURI());
+            BufferedImage bufferedImage = ImageIO.read(imageFile);
+            BufferedImage bufferedImageFrontView = bufferedImage.getSubimage(48, 7, 16, 25);
+            BufferedImage scaledBufferedImage = ImageProcessor.scaleImage(bufferedImageFrontView, 6);
+            javafx.scene.image.WritableImage writableImage = new javafx.scene.image.WritableImage(scaledBufferedImage.getWidth(), scaledBufferedImage.getHeight());
+            javafx.scene.image.PixelWriter pixelWriter = writableImage.getPixelWriter();
+            for (int y = 0; y < scaledBufferedImage.getHeight(); y++) {
+                for (int x = 0; x < scaledBufferedImage.getWidth(); x++) {
+                    pixelWriter.setArgb(x, y, scaledBufferedImage.getRGB(x, y));
+                }
+            }
+            return writableImage;
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private static String convertToBase64(BufferedImage image) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ImageIO.write(image, "png", baos);
@@ -121,8 +149,8 @@ public class ImageProcessor {
 
         return Base64.getEncoder().encodeToString(imageBytes);
     }
-  
-  public static Image resonseBodyToJavaFXImage(ResponseBody responseBody) throws IOException {
+
+    public static Image resonseBodyToJavaFXImage(ResponseBody responseBody) throws IOException {
         if (responseBody.source() != null) {
             try (InputStream inputStream = responseBody.byteStream()) {
                 byte[] imageData = toByteArray(inputStream);
