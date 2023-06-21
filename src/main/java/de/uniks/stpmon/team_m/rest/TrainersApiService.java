dc37c09968198f99335edcf809bb42844f1dab66
Cheng-JY <chengjiaying.prc@gmail.com>
2023-06-21 21:14:49 +0200

    Feat: Update the TrainersApiService

--- src/main/java/de/uniks/stpmon/team_m/rest/TrainersApiService.java
+++ src/main/java/de/uniks/stpmon/team_m/rest/TrainersApiService.java
@@ -2,6 +2,7 @@ package de.uniks.stpmon.team_m.rest;
 
 import de.uniks.stpmon.team_m.dto.CreateTrainerDto;
 import de.uniks.stpmon.team_m.dto.Trainer;
+import de.uniks.stpmon.team_m.dto.UpdateTrainerDto;
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.*;
 
@@ -14,6 +15,8 @@ public interface TrainersApiService {
     Observable<List<Trainer>> getTrainers(@Path("regionId") String regionId, @Query("area") String area, @Query("user") String id);
     @GET("regions/{regionId}/trainers/{id}")
     Observable<Trainer> getTrainer(@Path("regionId") String regionId, @Path("id") String _id);
+    @PATCH("regions/{regionId}/trainers/{id}")
+    Observable<Trainer> updateTrainer(@Path("regionId") String regionId, @Path("id") String _id, @Body UpdateTrainerDto dto);
     @DELETE("regions/{regionId}/trainers/{id}")
     Observable<Trainer> deleteTrainer(@Path("regionId") String regionId, @Path("id") String _id);
 }
