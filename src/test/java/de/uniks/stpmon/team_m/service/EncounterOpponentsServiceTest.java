204f9557e242683b53f1b2b8662f5f4462ae8123
Cheng-JY <chengjiaying.prc@gmail.com>
2023-06-22 17:25:51 +0200

    Chore: Refacting

--- src/test/java/de/uniks/stpmon/team_m/service/EncounterOpponentsServiceTest.java
+++ src/test/java/de/uniks/stpmon/team_m/service/EncounterOpponentsServiceTest.java
@@ -12,8 +12,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import javax.inject.Inject;
-
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
