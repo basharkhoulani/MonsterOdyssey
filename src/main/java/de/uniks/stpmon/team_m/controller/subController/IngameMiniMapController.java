d0a539928e1c7552ba75694999253d3a42502ee7
XadorXyves <uk070539@student.uni-kassel.de>
2023-06-24 14:22:59 +0200

    fix: disable actions while showing map and map ui changes

--- src/main/java/de/uniks/stpmon/team_m/controller/subController/IngameMiniMapController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/IngameMiniMapController.java
@@ -5,18 +5,14 @@ import de.uniks.stpmon.team_m.controller.IngameController;
 import de.uniks.stpmon.team_m.utils.TrainerStorage;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 
 public class IngameMiniMapController extends Controller {
-    @FXML
-    public Button closeButton;
     @FXML
     public ImageView mapImageView;
     @FXML
@@ -36,11 +32,5 @@ public class IngameMiniMapController extends Controller {
         final Parent parent = super.render();
         regionName.setText(trainerStorageProvider.get().getRegion().name());
         return parent;
-    }
-
-    public void closeMap() {
-        StackPane root = (StackPane) mapAnchorPane.getParent().getParent();
-        root.getChildren().remove(1);
-        root.getChildren().get(0).setEffect(null);
     }
 }
