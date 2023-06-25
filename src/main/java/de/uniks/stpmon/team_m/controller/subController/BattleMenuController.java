575f210aacc9120429cc680462e3ff1bc9f3b0a1
Cheng-JY <chengjiaying.prc@gmail.com>
2023-06-25 14:34:41 +0200

    feat: Add BattleMenu.fxml and Controller

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/BattleMenuController.java
@@ -0,0 +1,11 @@
+package de.uniks.stpmon.team_m.controller.subController;
+
+import de.uniks.stpmon.team_m.controller.Controller;
+import javafx.scene.control.Button;
+
+public class BattleMenuController extends Controller {
+
+    public Button abilitiesButton;
+    public Button changeMonsterButton;
+    public Button currentInfoButton;
+}
