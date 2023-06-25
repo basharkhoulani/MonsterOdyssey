db024502702bca8ddf4ed432ea25f271f8deaff1
Cheng-JY <chengjiaying.prc@gmail.com>
2023-06-25 14:21:44 +0200

    feat: Add EncounterController

--- src/main/java/de/uniks/stpmon/team_m/controller/EncounterController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/EncounterController.java
@@ -1,25 +1,49 @@
 package de.uniks.stpmon.team_m.controller;
 
+import de.uniks.stpmon.team_m.service.PresetsService;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
+import javax.inject.Inject;
+import javax.inject.Provider;
+
 public class EncounterController extends Controller{
+    @FXML
     public Label opponentLevel;
+    @FXML
     public ProgressBar opponentHealthBar;
+    @FXML
     public Label opponentMonsterName;
+    @FXML
     public ImageView opponentTrainer;
+    @FXML
     public ImageView opponentMonster;
+    @FXML
     public ImageView myMonster;
+    @FXML
     public ImageView mySprite;
+    @FXML
     public ProgressBar myLevelBar;
+    @FXML
     public Label myLevel;
+    @FXML
     public ProgressBar myHealthBar;
+    @FXML
     public Label myHealth;
+    @FXML
     public Label myMonsterName;
+    @FXML
     public HBox battleMenu;
+    @FXML
     public Text battleDescription;
+
+    @Inject
+    Provider<IngameController> ingameControllerProvider;
+    PresetsService presetsService;
+
 }
     
