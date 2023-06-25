8e7c5086291a25133405128442908e946b436270
Cheng-JY <chengjiaying.prc@gmail.com>
2023-06-25 13:44:53 +0200

    feat: Add EncounterController.java

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/EncounterController.java
@@ -0,0 +1,25 @@
+package de.uniks.stpmon.team_m.controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+public class EncounterController extends Controller{
+    public Label opponentLevel;
+    public ProgressBar opponentHealthBar;
+    public Label opponentMonsterName;
+    public ImageView opponentTrainer;
+    public ImageView opponentMonster;
+    public ImageView myMonster;
+    public ImageView mySprite;
+    public ProgressBar myLevelBar;
+    public Label myLevel;
+    public ProgressBar myHealthBar;
+    public Label myHealth;
+    public Label myMonsterName;
+    public HBox battleMenu;
+    public Text battleDescription;
+}
+    
