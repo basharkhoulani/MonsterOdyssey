79bb6c1322ba38526367ec7226b047d9b3e8c99e
Cheng-JY <chengjiaying.prc@gmail.com>
2023-06-25 21:12:49 +0200

    feat: add test

---
+++ src/test/java/de/uniks/stpmon/team_m/controller/EncounterControllerTest.java
@@ -0,0 +1,30 @@
+package de.uniks.stpmon.team_m.controller;
+
+import de.uniks.stpmon.team_m.App;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EncounterControllerTest extends ApplicationTest {
+
+    @InjectMocks
+    EncounterController encounterController;
+    @Spy
+    App app = new App(null);
+
+    public void start(Stage stage) {
+        ResourceBundle bundle = ResourceBundle.getBundle("de/uniks/stpmon/team_m/lang/lang", Locale.forLanguageTag("en"));
+        encounterController.setValues(bundle, null, null, encounterController, app);
+        app.start(stage);
+        app.show(encounterController);
+        stage.requestFocus();
+    }
+
+}
