f727a8b97e16e53aef6ae5bbf5814b3a8cbd2e95
Harun Dalkiran <harundlkrn46@gmail.com>
2023-06-26 11:39:18 +0200

    feat: add service for audio control

---
+++ src/main/java/de/uniks/stpmon/team_m/service/AudioService.java
@@ -0,0 +1,71 @@
+package de.uniks.stpmon.team_m.service;
+import de.uniks.stpmon.team_m.Main;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import javax.inject.Inject;
+
+public class AudioService {
+    private static AudioService instance;
+    private MediaPlayer mediaPlayer;
+    private boolean isMuted = false;
+    private String currentSound;
+
+    @Inject
+    public AudioService() {}
+
+    public static AudioService getInstance() {
+        if (instance == null) {
+            instance = new AudioService();
+        }
+        return instance;
+    }
+
+    public void playSound(String soundPath) {
+        final Media sound = new Media((Main.class.getResource("sounds/" + soundPath)).toString());
+        mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+        this.currentSound = soundPath;
+
+    }
+
+    public void stopSound() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            //isPaused = true;
+        }
+    }
+
+    public void muteSound() {
+        this.isMuted = true;
+        mediaPlayer.setMute(true);
+    }
+
+    public boolean checkMuted() {
+        return this.isMuted;
+    }
+
+    public void unmuteSound() {
+        this.isMuted = false;
+        mediaPlayer.setMute(false);
+    }
+
+    public String getCurrentSound() {
+        return this.currentSound;
+    }
+
+    public void pauseSound() {
+        if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+            mediaPlayer.pause();
+        }
+    }
+
+    public double getVolume() {
+        return mediaPlayer.getVolume();
+    }
+
+    public void setVolume(double volume) {
+        mediaPlayer.setVolume(volume);
+    }
+}
