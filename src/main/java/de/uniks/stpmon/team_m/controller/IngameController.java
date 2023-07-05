8ae12526c23eaf202b56af4f041fc0832a86fd83
Cheng-JY <chengjiaying.prc@gmail.com>
2023-07-05 20:26:44 +0200

    fix: disable the movement when showing Infowindow

--- src/main/java/de/uniks/stpmon/team_m/controller/IngameController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/IngameController.java
@@ -1068,6 +1068,7 @@ public class IngameController extends Controller {
         TextFlow dialogTextFlow = createDialogVBox(true);
         dialogTextFlow.getChildren().add(new Text(resources.getString("ENCOUNTER.INFO")));
         this.inEncounterInfoBox = true;
+        movementDisabled = true;
     }
 
     private void showEncounterScene() {
