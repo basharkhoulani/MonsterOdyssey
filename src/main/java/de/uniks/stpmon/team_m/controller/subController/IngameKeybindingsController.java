6010f87c15e3e6e4205d8c5d9f145d1952d599f6
Katharina136 <57876671+Katharina136@users.noreply.github.com>
2023-07-06 18:46:18 +0200

    feat: Controller for Ingame Keybindings

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/IngameKeybindingsController.java
@@ -0,0 +1,55 @@
+package de.uniks.stpmon.team_m.controller.subController;
+
+import de.uniks.stpmon.team_m.controller.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class IngameKeybindingsController extends Controller {
+
+    @FXML
+    public Button walkUpButton;
+    @FXML
+    public Button walkDownButton;
+    @FXML
+    public Button walkRightButton;
+    @FXML
+    public Button walkLeftButton;
+    @FXML
+    public Button interactionButton;
+    @FXML
+    public Button pauseMenuButton;
+    @FXML
+    public Button goBackButton;
+    @FXML
+    public Button defaultButton;
+    @FXML
+    public Button checkButton;
+
+
+    public void goBack() {
+    }
+
+    public void setDefault() {
+    }
+
+    public void check() {
+    }
+
+    public void setWalkLeft() {
+    }
+
+    public void setPauseMenu() {
+    }
+
+    public void setWalkRight() {
+    }
+
+    public void setInteraction() {
+    }
+
+    public void setWalkDown() {
+    }
+
+    public void setWalkUp() {
+    }
+}
