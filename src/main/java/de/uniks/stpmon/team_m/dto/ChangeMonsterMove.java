a9f87fd31b7c4f3243add48de972f914af21c90b
Cheng-JY <chengjiaying.prc@gmail.com>
2023-07-09 12:23:27 +0200

    fix: add annotation to resolve abstract types either need to be mapped to concrete types

--- src/main/java/de/uniks/stpmon/team_m/dto/ChangeMonsterMove.java
+++ src/main/java/de/uniks/stpmon/team_m/dto/ChangeMonsterMove.java
@@ -1,5 +1,8 @@
 package de.uniks.stpmon.team_m.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+@JsonTypeName("change-Monster")
 public record ChangeMonsterMove(
         String type,
         String monster
