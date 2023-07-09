6a8baab394689e6e67c6a861cff38bd537a3a96a
Katharina136 <57876671+Katharina136@users.noreply.github.com>
2023-07-09 13:17:56 +0200

    fix: Waiting for input works better

--- src/main/java/de/uniks/stpmon/team_m/controller/subController/IngameKeybindingsController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/IngameKeybindingsController.java
@@ -3,14 +3,12 @@ package de.uniks.stpmon.team_m.controller.subController;
 import de.uniks.stpmon.team_m.controller.Controller;
 import de.uniks.stpmon.team_m.controller.IngameController;
 import javafx.event.EventHandler;
-import de.uniks.stpmon.team_m.App;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.input.KeyEvent;
-
 import javax.inject.Inject;
 
 public class IngameKeybindingsController extends Controller {
@@ -66,90 +64,162 @@ public class IngameKeybindingsController extends Controller {
     public void setWalkLeft() {
         keyPressedHandler = event -> {
             if(Character.isLetterOrDigit(event.getText().charAt(0))){
-                informationLabel.setText("Walk left keybinding successfully changed!");
+                informationLabel.setText(resources.getString("CLICK.CHECK"));
                 walkLeftButton.setText("");
                 walkLeftButton.setText(event.getText().toUpperCase());
             }else{
-                informationLabel.setText("Ungültige Eingabe, versuche es erneut");
+                informationLabel.setText(resources.getString("WRONG.INPUT"));
+                walkLeftButton.setText("A");
             }
+            walkLeftButton.removeEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
+            walkUpButton.setDisable(false);
+            walkRightButton.setDisable(false);
+            walkDownButton.setDisable(false);
+            pauseMenuButton.setDisable(false);
+            interactionButton.setDisable(false);
         };
+        walkUpButton.setDisable(true);
+        walkRightButton.setDisable(true);
+        walkDownButton.setDisable(true);
+        pauseMenuButton.setDisable(true);
+        interactionButton.setDisable(true);
         walkLeftButton.setText("...");
-        informationLabel.setText("Waiting for input...");
+        informationLabel.setText(resources.getString("WAITING.INPUT"));
         walkLeftButton.addEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
     }
 
     public void setPauseMenu() {
         keyPressedHandler = event -> {
             if(Character.isLetterOrDigit(event.getText().charAt(0))){
-                informationLabel.setText("Pause Menu keybinding successfully changed!");
+                informationLabel.setText(resources.getString("CLICK.CHECK"));
                 pauseMenuButton.setText("");
                 pauseMenuButton.setText(event.getText().toUpperCase());
             }else{
-                informationLabel.setText("Ungültige Eingabe, versuche es erneut");
+                informationLabel.setText(resources.getString("WRONG.INPUT"));
+                pauseMenuButton.setText("ESC");
             }
+            pauseMenuButton.removeEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
+            walkUpButton.setDisable(false);
+            walkRightButton.setDisable(false);
+            walkDownButton.setDisable(false);
+            walkLeftButton.setDisable(false);
+            interactionButton.setDisable(false);
         };
-        pauseMenuButton.setText("...");
-        informationLabel.setText("Waiting for input...");
+        walkUpButton.setDisable(true);
+        walkRightButton.setDisable(true);
+        walkDownButton.setDisable(true);
+        walkLeftButton.setDisable(true);
+        interactionButton.setDisable(true);
+        walkLeftButton.setText("...");
+        informationLabel.setText(resources.getString("WAITING.INPUT"));
         pauseMenuButton.addEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
     }
 
     public void setWalkRight() {
         keyPressedHandler = event -> {
             if(Character.isLetterOrDigit(event.getText().charAt(0))){
-                informationLabel.setText("Walk right keybinding successfully changed!");
+                informationLabel.setText(resources.getString("CLICK.CHECK"));
                 walkRightButton.setText("");
                 walkRightButton.setText(event.getText().toUpperCase());
             }else{
-                informationLabel.setText("Ungültige Eingabe, versuche es erneut");
+                informationLabel.setText(resources.getString("WRONG.INPUT"));
+                walkRightButton.setText("D");
             }
+            walkRightButton.removeEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
+            walkUpButton.setDisable(false);
+            pauseMenuButton.setDisable(false);
+            walkDownButton.setDisable(false);
+            walkLeftButton.setDisable(false);
+            interactionButton.setDisable(false);
         };
+        walkUpButton.setDisable(true);
+        walkLeftButton.setDisable(true);
+        walkDownButton.setDisable(true);
+        pauseMenuButton.setDisable(true);
+        interactionButton.setDisable(true);
         walkRightButton.setText("...");
-        informationLabel.setText("Waiting for input...");
+        informationLabel.setText(resources.getString("WAITING.INPUT"));
         walkRightButton.addEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
     }
 
     public void setInteraction() {
         keyPressedHandler = event -> {
             if(Character.isLetterOrDigit(event.getText().charAt(0))){
-                informationLabel.setText("Interaction keybinding successfully changed!");
+                informationLabel.setText(resources.getString("CLICK.CHECK"));
                 interactionButton.setText("");
                 interactionButton.setText(event.getText().toUpperCase());
             }else{
-                informationLabel.setText("Ungültige Eingabe, versuche es erneut");
+                informationLabel.setText(resources.getString("WRONG.INPUT"));
+                interactionButton.setText("E");
             }
+            interactionButton.removeEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
+            walkUpButton.setDisable(false);
+            pauseMenuButton.setDisable(false);
+            walkDownButton.setDisable(false);
+            walkLeftButton.setDisable(false);
+            walkRightButton.setDisable(false);
         };
+        walkUpButton.setDisable(true);
+        walkLeftButton.setDisable(true);
+        walkDownButton.setDisable(true);
+        pauseMenuButton.setDisable(true);
+        walkRightButton.setDisable(true);
         interactionButton.setText("...");
-        informationLabel.setText("Waiting for input...");
+        informationLabel.setText(resources.getString("WAITING.INPUT"));
         interactionButton.addEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
     }
 
     public void setWalkDown() {
         keyPressedHandler = event -> {
             if(Character.isLetterOrDigit(event.getText().charAt(0))){
-                informationLabel.setText("Walk down keybinding successfully changed!");
+                informationLabel.setText(resources.getString("CLICK.CHECK"));
                 walkDownButton.setText("");
                 walkDownButton.setText(event.getText().toUpperCase());
             }else{
-                informationLabel.setText("Ungültige Eingabe, versuche es erneut");
+                informationLabel.setText(resources.getString("WRONG.INPUT"));
+                walkDownButton.setText("S");
             }
+            walkDownButton.removeEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
+            walkUpButton.setDisable(false);
+            pauseMenuButton.setDisable(false);
+            interactionButton.setDisable(false);
+            walkLeftButton.setDisable(false);
+            walkRightButton.setDisable(false);
         };
+        walkUpButton.setDisable(true);
+        walkRightButton.setDisable(true);
+        walkLeftButton.setDisable(true);
+        pauseMenuButton.setDisable(true);
+        interactionButton.setDisable(true);
         walkDownButton.setText("...");
-        informationLabel.setText("Waiting for input...");
+        informationLabel.setText(resources.getString("WAITING.INPUT"));
         walkDownButton.addEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
     }
 
     public void setWalkUp() {
         keyPressedHandler = event -> {
             if(Character.isLetterOrDigit(event.getText().charAt(0))){
-                informationLabel.setText("Walk up keybinding successfully changed!");
+                informationLabel.setText(resources.getString("CLICK.CHECK"));
                 walkUpButton.setText("");
                 walkUpButton.setText(event.getText().toUpperCase());
             }else{
-                informationLabel.setText("Ungültige Eingabe, versuche es erneut");
+                informationLabel.setText(resources.getString("WRONG.INPUT"));
+                walkUpButton.setText("W");
             }
+            walkUpButton.removeEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
+            walkDownButton.setDisable(false);
+            pauseMenuButton.setDisable(false);
+            interactionButton.setDisable(false);
+            walkLeftButton.setDisable(false);
+            walkRightButton.setDisable(false);
         };
+        walkDownButton.setDisable(true);
+        walkRightButton.setDisable(true);
+        walkLeftButton.setDisable(true);
+        pauseMenuButton.setDisable(true);
+        interactionButton.setDisable(true);
         walkUpButton.setText("...");
-        informationLabel.setText("Waiting for input...");
+        informationLabel.setText(resources.getString("WAITING.INPUT"));
         walkUpButton.addEventHandler(KeyEvent.KEY_PRESSED,keyPressedHandler);
     }
 }
