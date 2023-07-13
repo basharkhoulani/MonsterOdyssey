f0fbf93b875c200c8764a6db750057ee0f60334a
Cheng-JY <chengjiaying.prc@gmail.com>
2023-07-13 18:41:56 +0200

    feat: Add BattleMenuControllerTest

---
+++ src/test/java/de/uniks/stpmon/team_m/controller/subController/BattleMenuControllerTest.java
@@ -0,0 +1,79 @@
+package de.uniks.stpmon.team_m.controller.subController;
+
+import de.uniks.stpmon.team_m.App;
+import de.uniks.stpmon.team_m.controller.EncounterController;
+import de.uniks.stpmon.team_m.dto.Monster;
+import de.uniks.stpmon.team_m.dto.MonsterAttributes;
+import de.uniks.stpmon.team_m.service.EncounterOpponentsService;
+import de.uniks.stpmon.team_m.service.PresetsService;
+import de.uniks.stpmon.team_m.utils.EncounterOpponentStorage;
+import io.reactivex.rxjava3.core.Observable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import javax.inject.Provider;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class BattleMenuControllerTest extends ApplicationTest {
+
+    @Spy
+    App app = new App(null);
+
+    @InjectMocks
+    BattleMenuController battleMenuController;
+
+    @Mock
+    EncounterOpponentsService encounterOpponentsService;
+    @Mock
+    EncounterOpponentStorage encounterOpponentStorage;
+    @Mock
+    EncounterController encounterController;
+
+    @Override
+    public void start(Stage stage){
+        ResourceBundle bundle = ResourceBundle.getBundle("de/uniks/stpmon/team_m/lang/lang", Locale.forLanguageTag("en"));
+        battleMenuController.setValues(bundle, null, null, battleMenuController, app);
+
+        battleMenuController.init(encounterController, encounterOpponentStorage, app);
+        app.start(stage);
+        app.show(battleMenuController);
+        stage.requestFocus();
+    }
+
+    @Test
+    void controllerTest(){
+        final Button abilitiesButton = lookup("#abilitiesButton").query();
+        final Button changeMonsterButton = lookup("#changeMonsterButton").query();
+        final Button currentInfoButton = lookup("#currentInfoButton").query();
+        final Button fleeButton = lookup("#fleeButton").query();
+
+        assertTrue(abilitiesButton.isVisible());
+        assertTrue(changeMonsterButton.isVisible());
+        assertTrue(currentInfoButton.isVisible());
+        assertFalse(fleeButton.isVisible());
+    }
+
+    @Test
+    void showAbilitiesTest(){
+        final Button abilitiesButton = lookup("#abilitiesButton").query();
+        doNothing().when(encounterController).showAbilities();
+        clickOn(abilitiesButton);
+        verify(encounterController).showAbilities();
+    }
+
+
+}
