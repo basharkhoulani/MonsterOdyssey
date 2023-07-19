b1a383c473ebf602ddd7e48dfd61f8b9aa8a4150
Harun Dalkiran <harundlkrn46@gmail.com>
2023-07-19 20:46:43 +0200

    feat: initialize item list

--- src/main/java/de/uniks/stpmon/team_m/controller/subController/ItemMenuController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/ItemMenuController.java
@@ -2,9 +2,11 @@ package de.uniks.stpmon.team_m.controller.subController;
 
 import de.uniks.stpmon.team_m.controller.Controller;
 import de.uniks.stpmon.team_m.controller.IngameController;
+import javafx.fxml.FXML;
 import javafx.scene.Parent;
 
 import javax.inject.Inject;
+import javax.swing.text.html.ListView;
 
 public class ItemMenuController extends Controller {
 
@@ -18,6 +20,9 @@ public class ItemMenuController extends Controller {
         this.ingameController = ingameController;
     }
 
+    public void initItemList() {
+
+    }
     @Override
     public Parent render() {
         return super.render();
