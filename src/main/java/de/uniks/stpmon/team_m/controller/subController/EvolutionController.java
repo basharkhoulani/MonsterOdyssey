231eca5b160ce2630baa19961f4468ffa9b96baa
XadorXyves <uk070539@student.uni-kassel.de>
2023-07-24 09:41:46 +0200

    refactor: refactored evolution fxml and controller

---
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/EvolutionController.java
@@ -0,0 +1,70 @@
+package de.uniks.stpmon.team_m.controller.subController;
+
+import de.uniks.stpmon.team_m.App;
+import de.uniks.stpmon.team_m.controller.Controller;
+import de.uniks.stpmon.team_m.controller.EncounterController;
+import de.uniks.stpmon.team_m.dto.Monster;
+import de.uniks.stpmon.team_m.dto.MonsterTypeDto;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextFlow;
+
+import javax.inject.Inject;
+import java.awt.*;
+import java.util.Objects;
+
+public class EvolutionController extends Controller {
+    @FXML
+    public VBox evolutionVBox;
+    @FXML
+    public TextFlow evolutionTextFlow;
+    @FXML
+    public ImageView oldMonsterImageView;
+    @FXML
+    public Label oldMonsterLabel;
+    @FXML
+    public ImageView newMonsterImageView;
+    @FXML
+    public Label newMonster;
+    @FXML
+    public Button okButton;
+    @FXML
+    public ImageView arrowImageView;
+    private VBox container;
+    private StackPane root;
+    private EncounterController encounterController;
+    private Monster monster;
+    private MonsterTypeDto monsterTypeDto;
+    private Monster oldMonster;
+
+    @Inject
+    public EvolutionController() {
+    }
+
+    public void init(VBox container, StackPane root, EncounterController encounterController, Monster currentMonster, MonsterTypeDto currentMonsterTypeDto, Monster oldMonster) {
+        this.container = container;
+        this.root = root;
+        this.encounterController = encounterController;
+        this.monster = currentMonster;
+        this.monsterTypeDto = currentMonsterTypeDto;
+        this.oldMonster = oldMonster;
+    }
+
+    public Parent render() {
+        final Parent parent = super.render();
+        if (!GraphicsEnvironment.isHeadless()) {
+            arrowImageView.setImage(new Image(Objects.requireNonNull(App.class.getResource("images/arrowRight.png")).toString()));
+        }
+        return parent;
+    }
+
+    public void okButtonPressed() {
+        root.getChildren().remove(container);
+    }
+}
