a3de9a52f9d15ddef16a70c582240de23887a0e5
XadorXyves <uk070539@student.uni-kassel.de>
2023-07-27 13:20:24 +0200

    test: fixed LevelUpControllerTest

--- src/test/java/de/uniks/stpmon/team_m/controller/subController/LevelUpControllerTest.java
+++ src/test/java/de/uniks/stpmon/team_m/controller/subController/LevelUpControllerTest.java
@@ -2,11 +2,14 @@ package de.uniks.stpmon.team_m.controller.subController;
 
 
 import de.uniks.stpmon.team_m.App;
-import de.uniks.stpmon.team_m.controller.EncounterController;
+import de.uniks.stpmon.team_m.dto.AbilityDto;
 import de.uniks.stpmon.team_m.dto.Monster;
 import de.uniks.stpmon.team_m.dto.MonsterAttributes;
 import de.uniks.stpmon.team_m.dto.MonsterTypeDto;
-import javafx.scene.control.Button;
+import de.uniks.stpmon.team_m.service.PresetsService;
+import io.reactivex.rxjava3.core.Observable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -18,9 +21,11 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testfx.framework.junit5.ApplicationTest;
 
-import javax.inject.Provider;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 
@@ -32,16 +37,25 @@ public class LevelUpControllerTest extends ApplicationTest {
     @InjectMocks
     LevelUpController levelUpController;
     @Mock
-    Provider<EncounterController> encounterControllerProvider;
+    PresetsService presetsService;
 
 
     @Override
     public void start(Stage stage) {
         ResourceBundle bundle = ResourceBundle.getBundle("de/uniks/stpmon/team_m/lang/lang", Locale.forLanguageTag("en"));
         levelUpController.setValues(bundle, null, null, levelUpController, app);
-        final EncounterController encounterController = mock(EncounterController.class);
-        //when(encounterControllerProvider.get()).thenReturn(encounterController);
-        //doNothing().when(app).show(encounterController);
+
+
+        when(presetsService.getMonster(anyInt())).thenReturn(Observable.just(
+                new MonsterTypeDto(
+                        1,
+                        "Salamander",
+                        "salamander.png",
+                        List.of("fire"),
+                        "A fire lizard. It's hot."
+                )
+        ));
+
         LinkedHashMap<String, Integer> abilities = new LinkedHashMap<>();
         abilities.put("1", 35);
         abilities.put("3", 20);
@@ -64,15 +78,19 @@ public class LevelUpControllerTest extends ApplicationTest {
                 "Flamander_1.png",
                 List.of("fire"),
                 "Flamander is a small, agile monster that lives in the hot deserts of the world.");
-
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(new VBox(), new HBox());
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(vBox);
         levelUpController.init(
-                new VBox(),
-                new StackPane(),
-                encounterController,
+                vBox,
+                stackPane,
                 monster,
                 monsterTypeDto,
                 monster,
-                new ArrayList<Integer>());
+                new ArrayList<Integer>(),
+                new ArrayList<AbilityDto>(),
+                true);
         app.start(stage);
         app.show(levelUpController);
         stage.requestFocus();
@@ -81,10 +99,15 @@ public class LevelUpControllerTest extends ApplicationTest {
 
     @Test
     public void levelUpPopUpTest() {
-
-        final Button okButton = lookup("#okButton").query();
-        clickOn(okButton);
+        final Label level = lookup("#levelLabel").query();
+        assertEquals(level.getText(), "1 -> 1");
+        final Label health = lookup("#healthLabel").query();
+        assertEquals(health.getText(), "14.0 -> 14.0");
+        final Label attack = lookup("#attackLabel").query();
+        assertEquals(attack.getText(), "8 -> 8");
+        final Label defense = lookup("#defenseLabel").query();
+        assertEquals(defense.getText(), "8 -> 8");
+        final Label speed = lookup("#speedLabel").query();
+        assertEquals(speed.getText(), "5 -> 5");
     }
-
-
 }
