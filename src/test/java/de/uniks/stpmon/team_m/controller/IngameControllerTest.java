22ac718503df2273b9924100a7648c9674662ad8
Fin <uk059783@student.uni-kassel.de>
2023-07-29 20:49:03 +0200

    fix: fixed IngameControllerTest

--- src/test/java/de/uniks/stpmon/team_m/controller/IngameControllerTest.java
+++ src/test/java/de/uniks/stpmon/team_m/controller/IngameControllerTest.java
@@ -314,6 +314,9 @@ public class IngameControllerTest extends ApplicationTest {
         when(eventListener.get().listen("encounters.*.trainers." + trainerStorageProvider.get().getTrainer()._id() + ".opponents.*.*", Opponent.class)).thenReturn(just(
                 new Event<>("encounters.*.trainers.6475e595ac3946b6a812d865,opponents.*.nothappening", null)));
 
+        when(eventListenerMock.listen("trainers.6475e595ac3946b6a812d865.monsters.*.*", Monster.class)).thenReturn(Observable.empty());
+        when(eventListenerMock.listen("trainers.6475e595ac3946b6a812d865.items.*.*", Item.class)).thenReturn(Observable.empty());
+
         when(encounterOpponentsService.getTrainerOpponents(anyString(), anyString())).thenReturn(Observable.just(List.of()));
         app.start(stage);
         app.show(ingameController);
