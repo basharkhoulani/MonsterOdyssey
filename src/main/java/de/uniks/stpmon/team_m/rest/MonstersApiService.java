50db0d231065b51df21d30dcadaa6098ffb0deec
basharkhoulani <uk083781@uni-kassel.de>
2023-07-31 14:12:35 +0200

    feat: Add delete monster server call

--- src/main/java/de/uniks/stpmon/team_m/rest/MonstersApiService.java
+++ src/main/java/de/uniks/stpmon/team_m/rest/MonstersApiService.java
@@ -2,6 +2,7 @@ package de.uniks.stpmon.team_m.rest;
 
 import de.uniks.stpmon.team_m.dto.Monster;
 import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
@@ -13,4 +14,7 @@ public interface MonstersApiService {
 
     @GET("regions/{regionId}/trainers/{trainerId}/monsters/{monsterId}")
     Observable<Monster> getMonster(@Path("regionId") String regionId, @Path("trainerId") String trainerId, @Path("monsterId") String monsterId);
+
+    @DELETE("regions/{regionId}/trainers/{trainerId}/monsters/{monsterId}")
+    Observable<Monster> deleteMonster(@Path("regionId") String regionId, @Path("trainerId") String trainerId, @Path("monsterId") String monsterId);
 }
