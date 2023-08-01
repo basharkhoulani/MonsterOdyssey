7fcdaace4135b4a4994c15e143cc7a40dc619161
Cheng-JY <chengjiaying.prc@gmail.com>
2023-08-01 15:27:14 +0200

    fix: fix the type of UseItemMove

--- src/main/java/de/uniks/stpmon/team_m/dto/UseItemMove.java
+++ src/main/java/de/uniks/stpmon/team_m/dto/UseItemMove.java
@@ -2,7 +2,7 @@ package de.uniks.stpmon.team_m.dto;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
-@JsonTypeName("item")
+@JsonTypeName("use-item")
 public record UseItemMove(
         String type,
         int item,
