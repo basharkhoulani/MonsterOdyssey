56d7b410d99fdf17b523d050bb17f8758638a60d
Bashar Khoulani <uk083781@student.uni-kassel.de>
2023-08-01 13:56:43 +0200

    feat: Add delete Trainer after loss

--- src/main/java/de/uniks/stpmon/team_m/controller/subController/EncounterResultController.java
+++ src/main/java/de/uniks/stpmon/team_m/controller/subController/EncounterResultController.java
@@ -4,6 +4,10 @@ import de.uniks.stpmon.team_m.App;
 import de.uniks.stpmon.team_m.controller.Controller;
 import de.uniks.stpmon.team_m.controller.EncounterController;
 import de.uniks.stpmon.team_m.controller.IngameController;
+import de.uniks.stpmon.team_m.controller.MainMenuController;
+import de.uniks.stpmon.team_m.dto.Trainer;
+import de.uniks.stpmon.team_m.service.TrainersService;
+import de.uniks.stpmon.team_m.utils.TrainerStorage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
@@ -23,6 +27,12 @@ public class EncounterResultController extends Controller {
     Provider<EncounterController> encounterController;
     @Inject
     Provider<IngameController> ingameControllerProvider;
+    @Inject
+    Provider<MainMenuController> mainMenuControllerProvider;
+    @Inject
+    Provider<TrainerStorage> trainerStorageProvider;
+    @Inject
+    TrainersService trainersService;
     private Boolean coinsEarned;
     private Integer coinsAmount;
 
@@ -36,11 +46,19 @@ public class EncounterResultController extends Controller {
 
     public void clickOK() {
         encounterController.get().destroy();
-        IngameController ingameController = ingameControllerProvider.get();
-        ingameController.setCoinsAmount(getCoinsAmount());
-        ingameController.setCoinsEarned(getCoinsEarned());
-        ingameController.setIsNewStart(false);
-        app.show(ingameController);
+        Trainer trainer = trainerStorageProvider.get().getTrainer();
+        if (trainer.settings() != null && trainer.settings().permaDeath() != null && trainer.settings().permaDeath()) {
+            disposables.add(trainersService.deleteTrainer(trainer.region(), trainer._id()).observeOn(FX_SCHEDULER).subscribe(trainer1 -> {
+                destroy();
+                app.show(mainMenuControllerProvider.get());
+            }));
+        } else {
+            IngameController ingameController = ingameControllerProvider.get();
+            ingameController.setCoinsAmount(getCoinsAmount());
+            ingameController.setCoinsEarned(getCoinsEarned());
+            ingameController.setIsNewStart(false);
+            app.show(ingameController);
+        }
     }
 
     public void setInformationText(String text) {
