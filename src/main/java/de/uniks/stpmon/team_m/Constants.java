6d4f08de12789eed01bf296241bf7b957181ceec
Fin <uk059783@student.uni-kassel.de>
2023-08-02 11:10:25 +0200

    fix: changes USE_ITEM_MOVE value to "item"

--- src/main/java/de/uniks/stpmon/team_m/Constants.java
+++ src/main/java/de/uniks/stpmon/team_m/Constants.java
@@ -316,7 +316,7 @@ public class Constants {
     }
 
     public static final String ITEM_ACTION_USE_ITEM = "use";
-    public static final String ITEM_ACTION_USE_ITEM_MOVE = "use-item";
+    public static final String ITEM_ACTION_USE_ITEM_MOVE = "item";
     public static final String ITEM_USAGE_EFFECT = "effect";
     public static final String ITEM_USAGE_ITEM_BOX = "itemBox";
     public static final String ITEM_USAGE_MONSTER_BOX = "monsterBox";
